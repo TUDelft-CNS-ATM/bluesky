42d177a6bc235ce822b1de90708a03f94923469c
jooste <jooste@users.noreply.github.com>
2015-09-19 11:25:16 +0200

    Added logic to load settings.cfg from the project root dir, and write one if it doesn't exist yet

--- bluesky/settings.py
+++ bluesky/settings.py
@@ -1,5 +1,4 @@
-#gui = 'pygame'
-gui = 'qtgl'
+gui = 'qtgl'  # options: 'qtgl', 'pygame'
 
 scenario_path = 'data/scenario'
 
@@ -8,3 +7,14 @@ simdt = 0.02
 
 # Simulation thread update rate [Hz]
 sim_update_rate = 50
+
+# Import conig settings from settings.cfg if this exists, if it doesn't create an initial config file
+import os
+if os.path.isfile('settings.cfg'):
+    print 'Reading config from settings.cfg'
+    execfile('settings.cfg')
+else:
+    print 'Writing initial config file settings.cfg'
+    with open(os.path.dirname(__file__) + '/settings.py') as fin, open('settings.cfg', 'w') as fout:
+        for i in range(10):
+            fout.write(fin.readline())
