c0c3b9f4caad2f2ff8e79533244fce30e0de5695
jooste <jooste@users.noreply.github.com>
2015-09-23 10:11:14 +0200

    Small fixes in event processing qtgl

--- bluesky/ui/qtgl/gui.py
+++ bluesky/ui/qtgl/gui.py
@@ -17,6 +17,43 @@ from uievents import PanZoomEvent, ACDataEvent, StackTextEvent, PanZoomEventType
 from radarwidget import RadarWidget
 import autocomplete as ac
 
+usage_hints = { 'CRE' : 'acid,type,lat,lon,hdg,alt,spd',
+                'POS' : 'acid',
+                'MOVE': 'acid,lat,lon,[alt],[hdg],[spd],[vspd]',
+                'DEL': 'acid',
+                'ALT': 'acid,alt',
+                'HDG': 'acid,hdg',
+                'SPD': 'acid,spd',
+                'NOM': 'acid',
+                'VS': 'acid,vs',
+                'ORIG': 'acid,apt',
+                'DEST': 'acid,apt',
+                'ZOOM': 'in/out',
+                'PAN': 'LEFT/RIGHT/UP/DOWN/acid/airport/navid',
+                'IC': 'IC/filename',
+                'SAVEIC': 'filename',
+                'DT': 'dt',
+                'AREA': 'lat0,lon0,lat1,lon1,[lowalt]',
+                'TAXI': 'ON/OFF',
+                'SWRAD': 'GEO/GRID/APT/VOR/WPT/LABEL/TRAIL,[dt]/[value]',
+                'TRAIL': 'ON/OFF,[delta_t]',
+                'MCRE': 'n,type/*,alt/*,spd/*,dest/*',
+                'DIST': 'lat1,lon1,lat2,lon2',
+                'LNAV': 'acid,ON/OFF',
+                'VNAV': 'acid,ON/OFF',
+                'ASAS': 'acid,ON/OFF',
+                'ADDWPT': 'acid,wpname/latlon,[alt],[spd],[afterwp]',
+                'DELWPT': 'acid,wpname',
+                'DIRECT': 'acid,wpname',
+                'LISTRTE': 'acid,[pagenr]',
+                'ND': 'acid',
+                'NAVDISP': 'acid',
+                'NOISE': 'ON/OFF',
+                'LINE': 'color,lat1,lon1,lat2,lon2',
+                'ENG': 'acid',
+                'DATAFEED': 'CONNECT,address,port'
+                }
+
 
 class Gui(QApplication):
     modes = ['Init', 'Operate', 'Hold', 'End']
@@ -64,6 +101,10 @@ class Gui(QApplication):
         self.exec_()
 
     def notify(self, receiver, event):
+        # Keep track if we have to update the commandline
+        prev_cmdline    = self.command_line
+        event_processed = False
+
         # Events from the simulation thread
         if receiver is self:
             if event.type() == PanZoomEventType:
@@ -136,24 +177,21 @@ class Gui(QApplication):
                 return super(Gui, self).notify(self.radarwidget, PanZoomEvent(PanZoomEvent.Zoom, zoom, origin))
 
             elif event.type() == QEvent.MouseButtonPress:
+                event_processed = True
                 # For mice we pan with control/command and mouse movement. Mouse button press marks the beginning of a pan
                 if event.modifiers() & Qt.ControlModifier:
                     self.prevmousepos = (event.x(), event.y())
 
                 else:
                     latlon  = self.radarwidget.pixelCoordsToLatLon(event.x(), event.y())
-                    cmdline = str(self.win.lineEdit.text())[2:]
-                    tostack, todisplay = radarclick(cmdline, latlon[0], latlon[1], self.acdata, self.navdb)
+                    tostack, todisplay = radarclick(self.command_line, latlon[0], latlon[1], self.acdata, self.navdb)
                     if len(todisplay) > 0:
-                        if todisplay[0] == '\n':
-                            self.win.lineEdit.setText(">>")
-                        self.win.lineEdit.insert(todisplay.strip())
-                        if todisplay[-1] == '\n':
-                            self.win.lineEdit.setText(">>")
+                        if '\n' in todisplay:
+                            self.command_line = ''
+                        else:
+                            self.command_line += todisplay
                         if len(tostack) > 0:
                             self.stack(tostack)
-                    event.accept()
-                    return True
 
             elif event.type() == QEvent.MouseMove and event.modifiers() & Qt.ControlModifier and event.buttons() & Qt.LeftButton:
                 pan = (0.003 * (event.y() - self.prevmousepos[1]), 0.003 * (self.prevmousepos[0] - event.x()))
@@ -162,6 +200,7 @@ class Gui(QApplication):
 
         # Other events
         if event.type() == QEvent.KeyPress:
+            event_processed = True
             if event.key() == Qt.Key_Backspace:
                 self.command_line = self.command_line[:-1]
 
@@ -197,44 +236,7 @@ class Gui(QApplication):
             elif event.key() >= Qt.Key_Space and event.key() <= Qt.Key_AsciiTilde:
                 self.command_line += str(event.text()).upper()
 
-            # Accept the event and update the commandline in the gui
-            event.accept()
-            usage_hints = { 'CRE' : 'acid,type,lat,lon,hdg,alt,spd',
-                            'POS' : 'acid',
-                            'MOVE': 'acid,lat,lon,[alt,hdg,spd,vspd]',
-                            'DEL': 'acid',
-                            'ALT': 'acid,alt',
-                            'HDG': 'acid,hdg',
-                            'SPD': 'acid,spd',
-                            'NOM': 'acid',
-                            'VS': 'acid,vs',
-                            'ORIG': 'acid,apt',
-                            'DEST': 'acid,apt',
-                            'ZOOM': 'in/out',
-                            'PAN': 'LEFT/RIGHT/UP/DOWN/acid/airport/navid',
-                            'IC': 'IC/filename',
-                            'SAVEIC': 'filename',
-                            'DT': 'dt',
-                            'AREA': 'lat0,lon0,lat1,lon1[,lowalt]',
-                            'TAXI': 'ON/OFF',
-                            'SWRAD': 'GEO/GRID/APT/VOR/WPT/LABEL/TRAIL [dt]/[value]',
-                            'TRAIL': 'ON/OFF [dt]/TRAIL acid color',
-                            'MCRE': 'n, type/*, alt/*, spd/*, dest/*',
-                            'DIST': 'lat1,lon1,lat2,lon2',
-                            'LNAV': 'acid,ON/OFF',
-                            'VNAV': 'acid,ON/OFF',
-                            'ASAS': 'acid,ON/OFF',
-                            'ADDWPT': 'acid, (wpname/lat,lon),[alt],[spd],[afterwp]',
-                            'DELWPT': 'acid, wpname',
-                            'DIRECT': 'acid, wpname',
-                            'LISTRTE': 'acid,[pagenr]',
-                            'ND': 'acid',
-                            'NAVDISP': 'acid',
-                            'NOISE': 'ON/OFF',
-                            'LINE': 'color,lat1,lon1,lat2,lon2',
-                            'ENG': 'acid',
-                            'DATAFEED': 'CONNECT,address,port'
-                            }
+        if self.command_line != prev_cmdline:
             cmdline = self.command_line
             # Use both comma and space as a separator: two commas mean an empty argument
             while cmdline.find(",,") >= 0:
@@ -261,11 +263,12 @@ class Gui(QApplication):
 
             self.win.lineEdit.setHtml('<font color="#00ff00">>>' + self.command_line + '</font><font color="#aaaaaa"> ' + hint + '</font>')
 
-            return True
-
-        else:
-            # Call Base Class Method to Continue Normal Event Processing
+        if not event_processed:
+            # We haven't processed the event: call Base Class Method to Continue Normal Event Processing
             return super(Gui, self).notify(receiver, event)
+
+        event.accept()
+        return True
 
     def stack(self, text):
         self.postEvent(self.simevent_target, StackTextEvent(text))
