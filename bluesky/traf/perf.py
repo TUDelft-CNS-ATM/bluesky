Fixed NaN and overflow errors in perf.py Fixed typos in stack

bluesky/traf/perf.py
@@ -414,8 +414,8 @@ class Perf():
         self.refcas = np.append(self.refcas, vtas2cas(coeffBS.cr_spd[self.coeffidx], 35000*ft)) # nominal cruise CAS
 
         # limits   
-        self.vm_to = np.append(self.vmto, coeffBS.vmto[self.coeffidx])
-        self.vm_ld = np.append(self.vmld, coeffBS.vmld[self.coeffidx])   
+        self.vm_to = np.append(self.vm_to, coeffBS.vmto[self.coeffidx])
+        self.vm_ld = np.append(self.vm_ld, coeffBS.vmld[self.coeffidx])   
         self.vmto = np.append(self.vmto, 0.0)
         self.vmic = np.append(self.vmic, 0.0)        
         self.vmcr = np.append(self.vmcr, 0.0)

bluesky/ui/qtgl/radarwidget.py
@@ -369,8 +369,12 @@ class RadarWidget(QGLWidget):
             color    = np.zeros((self.naircraft, 3), dtype=np.float32)
             for i in range(self.naircraft):
                 if np.isnan(data.tas[i]):
-                    print 'NaN in %d: %s' % (i, data.id[i])
+                    print 'TAS NaN in %d: %s' % (i, data.id[i])
                     data.tas[i] = 0.0
+
+                if np.isnan(data.alt[i]):
+                    print 'ALT NaN in %d: %s' % (i, data.id[i])
+                    data.alt[i] = 0.0
 
                 rawlabel += '%-6sFL%03d %-6d' % (data.id[i], int(data.alt[i] / ft / 100), int(data.tas[i] / kts))
                 confidx = data.iconf[i]