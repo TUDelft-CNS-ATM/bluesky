303dfb8457775a49e136de8fbd55e7ecf13df3fd
Joost Ellerbroek <jooste@users.noreply.github.com>
2018-03-05 10:38:07 +0100

    clean qtgl.init

--- bluesky/ui/qtgl/gui.py
+++ bluesky/ui/qtgl/gui.py
@@ -1,23 +1,71 @@
 """ QTGL Gui for BlueSky."""
 try:
     from PyQt5.QtCore import Qt, QEvent, QT_VERSION, QT_VERSION_STR
-    from PyQt5.QtWidgets import QApplication
+    from PyQt5.QtWidgets import QApplication, QErrorMessage
+    from PyQt5.QtOpenGL import QGLFormat
 
 except ImportError:
     from PyQt4.QtCore import Qt, QEvent, QT_VERSION, QT_VERSION_STR
-    from PyQt4.QtGui import QApplication
+    from PyQt4.QtGui import QApplication, QErrorMessage
+    from PyQt4.QtOpenGL import QGLFormat
 
 # Local imports
 from bluesky import settings
+from bluesky.ui.qtgl import guiio
+from bluesky.ui.qtgl.mainwindow import MainWindow, Splash
+from bluesky.ui.qtgl.customevents import NUMCUSTOMEVENTS
 
-from bluesky.ui.qtgl.mainwindow import MainWindow
-
-from . import guiio as io
 
 print(('Using Qt ' + QT_VERSION_STR + ' for windows and widgets'))
 
 # Register settings defaults
 settings.set_variable_defaults(scenario_path='scenario')
+
+gui = None
+
+def init():
+    global gui
+    if gui is None:
+        gui = Gui()
+        splash = Splash()
+
+        # Register our custom pan/zoom event
+        for etype in range(1000, 1000 + NUMCUSTOMEVENTS):
+            reg_etype = QEvent.registerEventType(etype)
+            if reg_etype != etype:
+                print(('Warning: Registered event type differs from requested type id (%d != %d)' % (reg_etype, etype)))
+
+        splash.show()
+
+        # Install error message handler
+        handler = QErrorMessage.qtHandler()
+        handler.setWindowFlags(Qt.WindowStaysOnTopHint)
+
+        # Check and set OpenGL capabilities
+        if not QGLFormat.hasOpenGL():
+            raise RuntimeError('No OpenGL support detected for this system!')
+        else:
+            f = QGLFormat()
+            f.setVersion(3, 3)
+            f.setProfile(QGLFormat.CoreProfile)
+            f.setDoubleBuffer(True)
+            QGLFormat.setDefaultFormat(f)
+            print(('QGLWidget initialized for OpenGL version %d.%d' % (f.majorVersion(), f.minorVersion())))
+
+
+        splash.showMessage('Constructing main window')
+        gui.processEvents()
+        gui.init()
+        guiio.init()
+        splash.showMessage('Done!')
+        gui.processEvents()
+        splash.finish(gui.win)
+
+
+def start():
+    init()
+    gui.start()
+
 
 class Gui(QApplication):
     def __init__(self):
@@ -40,7 +88,7 @@ class Gui(QApplication):
 
     def quit(self):
         # Send quit to server
-        io.send_event(b'QUIT')
+        guiio.send_event(b'QUIT')
         self.closeAllWindows()
 
     def notify(self, receiver, event):
