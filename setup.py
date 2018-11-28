4387d59c5b6bf6197ac3be96f9193d5522c37175
Liam Deacon <liam.m.deacon@gmail.com>
2018-11-28 17:17:15 +0000

    Pruned size of package

--- setup.py
+++ setup.py
@@ -52,7 +52,9 @@ setup(
     # "scripts" keyword. Entry points provide cross-platform support and allow
     # `pip` to create the appropriate form of executable for the target
     # platform.
-    data_files = [('/share/bluesky/data', [f for f in glob.glob('data/**/*') if path.isfile(f)] + ['data/default.cfg']),
+    data_files = [('/share/bluesky/data', [f for f in glob.glob('data/**/*') if path.isfile(f)
+                  and path.basename(f) not in ('world.16384x8192.dds',
+                                               'world.8192x4096.dds')] + ['data/default.cfg']),
                   ('/share/bluesky/plugins', [f for f in glob.glob('plugins/*.py') if path.isfile(f)]),
                   ('/share/bluesky/utils', [f for f in glob.glob('utils/**/*') if path.isfile(f)])],
     entry_points={
