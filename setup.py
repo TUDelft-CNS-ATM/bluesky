0d5bfaa7c5701e71333abdb292f645f1cb003393
Liam Deacon <liam.m.deacon@gmail.com>
2018-11-28 15:58:15 +0000

    Bump version, added data_files to packaging

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 # To use a consistent encoding
 from codecs import open
 from os import path
+import glob
 
 here = path.abspath(path.dirname(__file__))
 
@@ -10,10 +11,9 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-
 setup(
     name='bluesky-simulator',  # 'bluesky' package name already taken
-    version='0.0.1.dev1', 
+    version='0.0.1.dev2',
     description='The Open Air Traffic Simulator',
     long_description=long_description,
     url='https://github.com/pypa/sampleproject',
@@ -35,31 +35,41 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
 
     # This field adds keywords for your project which will appear on the
-    keywords='atm aft transport simulation',  
+    keywords='atm aft transport simulation',
     packages=find_packages(exclude=['contrib', 'docs', 'tests']),  # Required
     install_requires=open(here + '/requirements.txt', 'r').readlines(),
     extras_require={
         'dev': ['check-manifest'],
         'test': ['coverage', 'flake8', 'radon', 'nose'],
     },
+    include_package_data=True,
     package_data={
-        'scripts': [here + '/BlueSky_pygame.py', here + '/BlueSky_qtgl.py']
-    #    'sample': ['package_data.dat'],
+          'data': [f for f in glob.glob('data/**/*') if path.isfile(f)] + ['data/default.cfg']
     },
+    #     'scripts': [here + '/BlueSky_pygame.py', here + '/BlueSky_qtgl.py',
+    #                 here + '/utils/mkcustomnavdata/makescen.py'] + \
+    #                 glob.glob(here + '/utils/Scenario-creator/'),
+    #     '': glob.glob(here + '/plugins/*.py'),
+    #     '': glob.glob(here + '/data/**/*'),
+    # },
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
     # `pip` to create the appropriate form of executable for the target
     # platform.
+    data_files = [('/share/bluesky/data', [f for f in glob.glob('data/**/*') if path.isfile(f)] + ['data/default.cfg']),
+                  ('/share/bluesky/plugins', [f for f in glob.glob('plugins/*.py') if path.isfile(f)]),
+                  ('/share/bluesky/utils', [f for f in glob.glob('utils/**/*') if path.isfile(f)])],
     entry_points={
         'console_scripts': [
             'BlueSky=bluesky:main',
         ],
     },
 
-    scripts=[here + '/BlueSky_pygame.py', here + '/BlueSky_qtgl.py'],
+    scripts=['BlueSky_pygame.py', 'BlueSky_qtgl.py'],
 
     project_urls={
         'Source': 'https://github.com/ProfHoekstra/bluesky',
