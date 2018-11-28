c9236fd6c10d61286c736bd401f4cd1af700f21d
Liam Deacon <liam.m.deacon@gmail.com>
2018-11-28 16:51:38 +0000

    Fixed License classifier to meet PyPI standards

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
         'Topic :: Scientific/Engineering',
 
         # Pick your license as you wish
-        'License :: OSI Approved :: GPLv3 License',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
