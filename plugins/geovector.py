9ea457b45fc73cf7133b460c8245c132d73febfe
Joost Ellerbroek <jooste@users.noreply.github.com>
2019-05-21 16:16:56 +0200

    Update to enable reverting to original trajectory after exiting geovectored area

--- plugins/geovector.py
+++ plugins/geovector.py
@@ -11,13 +11,38 @@ Geovector is defined as:
  """
 
 
-from bluesky import stack,traf,sim,tools  #, settings, navdb, traf, sim, scr, tools
+from bluesky import traf  #, settings, navdb, traf, sim, scr, tools
 from bluesky.tools import areafilter
 from bluesky.tools.aero import vtas2cas,ft
 from bluesky.tools.misc import degto180
 
 
 import numpy as np
+
+
+# A dictionary of areas with a geovector specification
+geovecs = dict()
+
+
+class GeoVector:
+    ''' Geovector specification class. '''
+    def __init__(self, areaname, gsmin, gsmax,
+                 trkmin, trkmax,
+                 vsmin, vsmax):
+        self.areaname = areaname
+        self.gsmin = gsmin
+        self.gsmax = gsmax
+        self.trkmin = trkmin
+        self.trkmax = trkmax
+        self.vsmin = vsmin
+        self.vsmax = vsmax
+        self.previnside = set()
+        self.prevstatus = dict()
+
+    def __str__(self):
+        ''' Pretty printed geovector information. '''
+        return 'gs: {s.gsmin}-{s.gsmax} [m/s], {s.trkmin}-{s.trkmax} trk[deg], {s.vsmin}-{s.vsmax} vs[m/s]'.format(s=self)
+
 
 def init_plugin():
 
@@ -75,55 +100,68 @@ def preupdate(): # To be safe preupdate is used iso update
 
 def applygeovec():
     # Apply each geovector
-    for vec in geovecs:
-        areaname = vec[0]
+    for areaname, vec in geovecs.items():
         if areafilter.hasArea(areaname):
             swinside  = areafilter.checkInside(areaname, traf.lat, traf.lon, traf.alt)
 
-            gsmin,gsmax,trkmin,trkmax,vsmin,vsmax = vec[1:]
+            insids = set(np.array(traf.id)[swinside])
+            newids = insids - vec.previnside
+            delids = vec.previnside - insids
+            # Store LNAV/VNAV status of new aircraft
+            for acid in newids:
+                idx = traf.id2idx(acid)
+                vec.prevstatus[acid] = [traf.swlnav[idx], traf.swvnav[idx]]
 
+            # Revert aircraft who have exited the geovectored area to their original status
+            for acid in delids:
+                idx = traf.id2idx(acid)
+                if idx >= 0:
+                    traf.swlnav[idx], traf.swvnav[idx] = vec.prevstatus.pop(acid)
+            
+            vec.previnside = insids
             # -----Ground speed limiting
             # For now assume no wind:  so use tas as gs
-            if gsmin:
-                casmin = vtas2cas(np.ones(traf.ntraf)*gsmin,traf.alt)
-                usemin = traf.selspd<casmin
+            if vec.gsmin is not None:
+                casmin = vtas2cas(np.ones(traf.ntraf) * vec.gsmin, traf.alt)
+                usemin = traf.selspd < casmin
                 traf.selspd[swinside & usemin] = casmin[swinside & usemin]
+                traf.swvnav[swinside & usemin] = False
 
-            if gsmax:
-                casmax = vtas2cas(np.ones(traf.ntraf)*gsmax,traf.alt)
+            if vec.gsmax is not None:
+                casmax = vtas2cas(np.ones(traf.ntraf) * vec.gsmax, traf.alt)
                 usemax = traf.selspd > casmax
                 traf.selspd[swinside & usemax] = casmax[swinside & usemax]
+                traf.swvnav[swinside & usemax] = False
 
             #------ Limit Track(so hdg)
             # Max track interval is 180 degrees to avoid ambiguity of what is inside the interval
 
-            if trkmin and trkmax:
-
+            if None not in [vec.trkmin, vec.trkmax]:
                 # Use degto180 to avodi problems for e.g interval [350,30]
-                usemin = swinside & (degto180(traf.trk - trkmin)<0) # Left of minimum
-                usemax = swinside & (degto180(traf.trk - trkmax)>0) # Right of maximum
+                usemin = swinside & (degto180(traf.trk - vec.trkmin) < 0) # Left of minimum
+                usemax = swinside & (degto180(traf.trk - vec.trkmax) > 0) # Right of maximum
 
                 #print(usemin,usemax)
+                traf.swlnav[swinside & (usemin | usemax)] = False
 
-                traf.ap.trk[swinside & usemin] = trkmin
-                traf.ap.trk[swinside & usemax] = trkmax
+                traf.ap.trk[swinside & usemin] = vec.trkmin
+                traf.ap.trk[swinside & usemax] = vec.trkmax
 
             # -----Ground speed limiting
             # For now assume no wind:  so use tas as gs
-            if vsmin:
-
-                traf.selvs[swinside & (traf.vs<vsmin)] = vsmin
-
+            if vec.vsmin is not None:
+                traf.selvs[swinside & (traf.vs < vec.vsmin)] = vec.vsmin
+                traf.swvnav[swinside & (traf.vs < vec.vsmin)] = False
                 # Activate V/S mode by using a slightly higher altitude than current values
-                traf.selalt[swinside & (traf.vs < vsmin)] = traf.alt[swinside & (traf.vs < vsmin)] + \
-                                                            np.sign(vsmin)*200.*ft
+                traf.selalt[swinside & (traf.vs < vec.vsmin)] = traf.alt[swinside & (traf.vs < vec.vsmin)] + \
+                                                            np.sign(vec.vsmin)*200.*ft
 
-            if vsmax:
-                traf.selvs[swinside & (traf.vs > vsmax)] = vsmax
-
+            if vec.vsmax is not None:
+                traf.selvs[swinside & (traf.vs > vec.vsmax)] = vec.vsmax
+                traf.swvnav[swinside & (traf.vs < vec.vsmax)] = False
                 # Activate V/S mode by using a slightly higher altitude than current values
-                traf.selalt[swinside & (traf.vs > vsmax)] = traf.alt[swinside & (traf.vs > vsmax)] + \
-                                                            np.sign(vsmax)*200.*ft
+                traf.selalt[swinside & (traf.vs > vec.vsmax)] = traf.alt[swinside & (traf.vs > vec.vsmax)] + \
+                                                            np.sign(vec.vsmax)*200.*ft
 
     return
 
@@ -131,79 +169,55 @@ def update(): # Not used
     return
 
 def reset():
-    global geovecs
-    geovecs = [] # [[area,gsmin,gsmax,trkin,trkmax,vsmin,vsmax]]
+    geovecs.clear() # [[area,gsmin,gsmax,trkin,trkmax,vsmin,vsmax]]
     return
 
 ### Other functions of your plug-in
 
-def defgeovec(area="", spdmin=None, spdmax=None, trkmin=None, trkmax=None, vspdmin=None, vspdmax=None):
-    global geovecs
-    #print ("defgeovec input=",area,gsmin,gsmax,trkmin,trkmax,vspdmin,vspdmax,sep="|")
+def defgeovec(areaname="", spdmin=None, spdmax=None, trkmin=None, trkmax=None, vspdmin=None, vspdmax=None):
+    #print ("defgeovec input=",areaname,gsmin,gsmax,trkmin,trkmax,vspdmin,vspdmax,sep="|")
 
     # We need an area to do anything
-    if area=="":
-        return False,"We need an area"
+    if not areaname:
+        return False, "We need an area"
 
     if not (spdmin or spdmax or (trkmin and trkmax) or vspdmin or vspdmax):
-        i = 0
-        idx = -1
-        while i < len(geovecs):
-            if geovecs[i][0].upper() == area.upper():
-                idx = i
-                break
+        if areaname in geovecs:
+            return True, areaname + " uses " + str(geovecs[areaname])
+        return False, "No geovector found for " + areaname
 
-            i = i + 1
-        if idx>=0:
-            return True,area+" uses "+str(geovecs[idx][1:])+" gs[m/s], trk[deg], vs[m/s]"
-        else:
-            return False,"No geovector found for "+area
-
-
-    # Remove old geovector for this area
-    i = 0
-    while i<len(geovecs):
-        if geovecs[i][0].upper()==area.upper():
-            del geovecs[i]
-        else:
-            i = i + 1
+    # Remove old geovector for this area (if it exists)
+    geovecs.pop(areaname, None)
 
     # Only add it if an interval is given
     if spdmin or spdmax or (trkmin and trkmax) or vspdmin or vspdmax:
         # Allow wrong order of min and max (note for hdg, as 355,10 is valid range!)
-        if spdmin and spdmax:
+        if None not in [spdmin, spdmax]:
             gsmin = min(spdmin,spdmax)
             gsmax = max(spdmin,spdmax)
         else:
             gsmin = spdmin
             gsmax = spdmax
 
-        if vspdmin and vspdmax:
+        if None not in [vspdmin, vspdmax]:
             vsmin = min(vspdmin,vspdmax)
             vsmax = max(vspdmin,vspdmax)
         else:
             vsmin = vspdmin
             vsmax = vspdmax
 
-        # Add geovector to the list of geovectors
-        geovecs.append([area,
+        # Add geovector to the dict of geovectors
+        geovecs[areaname] = GeoVector(areaname,
                         gsmin, gsmax,
                         trkmin,trkmax,
-                        vsmin, vsmax ])
+                        vsmin, vsmax)
 
     return True
 
 def delgeovec(area=""):
-    found = False
-    i = 0
-    while i<len(geovecs):
-        if geovecs[i][0].upper()==area.upper():
-            found = True
-            del geovecs[i]
-        else:
-            i = i + 1
-
+    # Delete geovector specification for area if it exists
+    found = geovecs.pop(area)
     if not found:
-        return False,"No geovector found for "+area
-    else:
-        return True
+        return False, "No geovector found for " + area
+
+    return True, 'Deleted geovector specification for ' + area
