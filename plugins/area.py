6e6dfaabbdd0301eee5ed1516efd771d3534492c
Joost Ellerbroek <jooste@users.noreply.github.com>
2019-11-20 10:37:10 +0100

    Updated area to have separate deletion and experiment area definitions

--- plugins/area.py
+++ plugins/area.py
@@ -7,37 +7,46 @@ from bluesky import traf, sim  #, settings, navdb, traf, sim, scr, tools
 from bluesky.tools import datalog, areafilter, \
     TrafficArrays, RegisterElementParameters
 from bluesky.tools.aero import ft
-from bluesky import settings
+from bluesky.tools.simtime import timed_function
 
 # Log parameters for the flight statistics log
-header = \
-    "#######################################################\n" + \
-    "FLST LOG\n" + \
-    "Flight Statistics\n" + \
-    "#######################################################\n\n" + \
-    "Parameters [Units]:\n" + \
-    "Deletion Time [s], " + \
-    "Call sign [-], " + \
-    "Spawn Time [s], " + \
-    "Flight time [s], " + \
-    "Actual Distance 2D [m], " + \
-    "Actual Distance 3D [m], " + \
-    "Work Done [J], " + \
-    "Latitude [deg], " + \
-    "Longitude [deg], " + \
-    "Altitude [m], " + \
-    "TAS [m/s], " + \
-    "Vertical Speed [m/s], " + \
-    "Heading [deg], " + \
-    "Origin Lat [deg], " + \
-    "Origin Lon [deg], " + \
-    "Destination Lat [deg], " + \
-    "Destination Lon [deg], " + \
-    "ASAS Active [bool], " + \
-    "Pilot ALT [m], " + \
-    "Pilot SPD (TAS) [m/s], " + \
-    "Pilot HDG [deg], " + \
-    "Pilot VS [m/s]"  + "\n"
+flstheader = \
+    '#######################################################\n' + \
+    'FLST LOG\n' + \
+    'Flight Statistics\n' + \
+    '#######################################################\n\n' + \
+    'Parameters [Units]:\n' + \
+    'Deletion Time [s], ' + \
+    'Call sign [-], ' + \
+    'Spawn Time [s], ' + \
+    'Flight time [s], ' + \
+    'Actual Distance 2D [m], ' + \
+    'Actual Distance 3D [m], ' + \
+    'Work Done [J], ' + \
+    'Latitude [deg], ' + \
+    'Longitude [deg], ' + \
+    'Altitude [m], ' + \
+    'TAS [m/s], ' + \
+    'Vertical Speed [m/s], ' + \
+    'Heading [deg], ' + \
+    'Origin Lat [deg], ' + \
+    'Origin Lon [deg], ' + \
+    'Destination Lat [deg], ' + \
+    'Destination Lon [deg], ' + \
+    'ASAS Active [bool], ' + \
+    'Pilot ALT [m], ' + \
+    'Pilot SPD (TAS) [m/s], ' + \
+    'Pilot HDG [deg], ' + \
+    'Pilot VS [m/s]'  + '\n'
+
+confheader = \
+    '#######################################################\n' + \
+    'CONF LOG\n' + \
+    'Conflict Statistics\n' + \
+    '#######################################################\n\n' + \
+    'Parameters [Units]:\n' + \
+    'Simulation time [s], ' + \
+    'Total number of conflicts in exp area [-]\n'
 
 # Global data
 area = None
@@ -58,162 +67,210 @@ def init_plugin():
         # The type of this plugin. For now, only simulation plugins are possible.
         'plugin_type':     'sim',
 
-        # Update interval in seconds.
-        'update_interval': area.dt,
-
         # The update function is called after traffic is updated.
         'update':          area.update,
+
+        # The reset function
+        'reset':           area.reset
         }
 
     stackfunctions = {
-        "AREA": [
-            "AREA Shapename/OFF or AREA lat,lon,lat,lon,[top,bottom]",
-            "[float/txt,float,float,float,alt,alt]",
+        'AREA': [
+            'AREA Shapename/OFF or AREA lat,lon,lat,lon,[top,bottom]',
+            '[float/txt,float,float,float,alt,alt]',
             area.set_area,
-            "Define experiment area (area of interest)"
+            'Define deletion area (aircraft leaving area are deleted)'
         ],
-        "TAXI": [
-            "TAXI ON/OFF [alt] : OFF auto deletes traffic below 1500 ft",
-            "onoff[,alt]",
+        'EXP': [
+            'EXP Shapename/OFF or EXP lat,lon,lat,lon,[top,bottom]',
+            '[float/txt,float,float,float,alt,alt]',
+            lambda *args: area.set_area(*args, exparea=True),
+            'Define experiment area (area of interest)'
+        ],
+        'TAXI': [
+            'TAXI ON/OFF [alt] : OFF auto deletes traffic below 1500 ft',
+            'onoff[,alt]',
             area.set_taxi,
-            "Switch on/off ground/low altitude mode, prevents auto-delete at 1500 ft"
+            'Switch on/off ground/low altitude mode, prevents auto-delete at 1500 ft'
         ]
     }
     # init_plugin() should always return these two dicts.
     return config, stackfunctions
 
 class Area(TrafficArrays):
-    """ Traffic area: delete traffic when it leaves this area (so not when outside)"""
+    ''' Traffic area: delete traffic when it leaves this area (so not when outside)'''
     def __init__(self):
         super(Area, self).__init__()
         # Parameters of area
         self.active = False
-        self.dt     = 0.5     # [s] frequency of area check (simtime)
-        self.name   = None
+        self.delarea = ''
+        self.exparea = ''
         self.swtaxi = True  # Default OFF: Doesn't do anything. See comments of set_taxi fucntion below.
-        self.swtaxialt = 1500.  # Default OFF: Doesn't do anything. See comments of set_taxi fucntion below.
+        self.swtaxialt = 1500.0  # Default OFF: Doesn't do anything. See comments of set_taxi fucntion below.
+
+        self.confinside_all = 0
 
         # The FLST logger
-        self.logger = datalog.crelog('FLSTLOG', None, header)
+        self.flst = datalog.crelog('FLSTLOG', None, flstheader)
+        self.conflog = datalog.crelog('CONFLOG', None, confheader)
 
         with RegisterElementParameters(self):
-            self.inside      = np.array([],dtype = np.bool) # In test area or not
-            self.oldalt      = np.array([])
-            self.distance2D  = np.array([])
-            self.distance3D  = np.array([])
-            self.work        = np.array([])
-            self.create_time = np.array([])
+            self.insdel = np.array([], dtype=np.bool) # In deletion area or not
+            self.insexp = np.array([], dtype=np.bool) # In experiment area or not
+            self.oldalt = np.array([])
+            self.distance2D = np.array([])
+            self.distance3D = np.array([])
+            self.dstart2D = np.array([])
+            self.dstart3D = np.array([])
+            self.workstart = np.array([])
+            self.work = np.array([])
+            self.entrytime = np.array([])
+
+    def reset(self):
+        ''' Reset area state when simulation is reset. '''
+        super().reset()
+        self.active = False
+        self.delarea = ''
+        self.exparea = ''
+        self.swtaxi = True
+        self.swtaxialt = 1500.0
+        self.confinside_all = 0
 
     def create(self, n=1):
         super(Area, self).create(n)
-        self.create_time[-n:] = sim.simt
         self.oldalt[-n:] = traf.alt[-n:]
+        self.insdel[-n:] = False
+        self.insexp[-n:] = False
 
-    def update(self):
+    @timed_function('AREA', dt=1.0)
+    def update(self, dt):
         ''' Update flight efficiency metrics
             2D and 3D distance [m], and work done (force*distance) [J] '''
-        if self.swtaxi and not self.active:
-            return
+        if self.active:
+            resultantspd = np.sqrt(traf.gs * traf.gs + traf.vs * traf.vs)
+            self.distance2D += dt * traf.gs
+            self.distance3D += dt * resultantspd
+            self.work += (traf.perf.thrust * dt * resultantspd)
 
-        resultantspd = np.sqrt(traf.gs * traf.gs + traf.vs * traf.vs)
-        self.distance2D += self.dt * traf.gs
-        self.distance3D += self.dt * resultantspd
+            # Find out which aircraft are currently inside the experiment area, and
+            # determine which aircraft need to be deleted.
+            insdel = areafilter.checkInside(self.delarea, traf.lat, traf.lon, traf.alt)
+            insexp = insdel if not self.exparea else \
+                areafilter.checkInside(self.exparea, traf.lat, traf.lon, traf.alt)
+            # Find all aircraft that were inside in the previous timestep, but no
+            # longer are in the current timestep
+            delidx = np.where(np.array(self.insdel) * (np.array(insdel) == False))[0]
+            self.insdel = insdel
 
-        if settings.performance_model == 'openap':
-            self.work += (traf.perf.thrust * self.dt * resultantspd)
-        else:
-            self.work += (traf.perf.Thr * self.dt * resultantspd)
+            # Count new conflicts where at least one of the aircraft is inside
+            # the experiment area
+            if traf.asas.confpairs_new:
+                newconf_unique = {frozenset(pair)
+                                  for pair in traf.asas.confpairs_new}
+                ac1, ac2 = zip(*newconf_unique)
+                idx1 = traf.id2idx(ac1)
+                idx2 = traf.id2idx(ac2)
+                newconf_inside = np.logical_or(insexp[idx1], insexp[idx2])
+
+                nnewconf_exp = np.count_nonzero(newconf_inside)
+                if nnewconf_exp:
+                    self.confinside_all += nnewconf_exp
+                    self.conflog.log(self.confinside_all)
+
+            # Register distance values upon entry of experiment area
+            newentries = np.logical_not(self.insexp) * insexp
+            self.dstart2D[newentries] = self.distance2D[newentries]
+            self.dstart3D[newentries] = self.distance3D[newentries]
+            self.workstart[newentries] = self.work[newentries]
+            self.entrytime[newentries] = sim.simt
+
+            # Log flight statistics when exiting experiment area
+            exits = self.insexp * np.logical_not(insexp)
+            if np.any(exits):
+                self.flst.log(
+                    np.array(traf.id)[exits],
+                    self.create_time[exits],
+                    sim.simt - self.entrytime[exits],
+                    self.dstart2D[exits] - self.distance2D[exits],
+                    self.dstart3D[exits] - self.distance3D[exits],
+                    self.workstart[exits] - self.work[exits],
+                    traf.lat[exits],
+                    traf.lon[exits],
+                    traf.alt[exits],
+                    traf.tas[exits],
+                    traf.vs[exits],
+                    traf.hdg[exits],
+                    traf.asas.active[exits],
+                    traf.pilot.alt[exits],
+                    traf.pilot.tas[exits],
+                    traf.pilot.vs[exits],
+                    traf.pilot.hdg[exits])
+
+            # delete all aicraft in self.delidx
+            if len(delidx) > 0:
+                traf.delete(delidx)
 
         # Autodelete for descending with swTaxi:
         if not self.swtaxi:
-            #delidxalt = np.where(traf.alt<self.swtaxialt)[0]
-            delidxalt = np.where((self.oldalt>=self.swtaxialt)*(traf.alt<self.swtaxialt))[0]
+            delidxalt = np.where((self.oldalt >= self.swtaxialt)
+                                 * (traf.alt < self.swtaxialt))[0]
             self.oldalt = traf.alt
-        else:
-            delidxalt = []
+            if len(delidxalt) > 0:
+                traf.delete(list(delidxalt))
 
-
-
-        # Find out which aircraft are currently inside the experiment area, and
-        # determine which aircraft need to be deleted.
-        inside = areafilter.checkInside(self.name, traf.lat, traf.lon, traf.alt)
-        #delidx = np.intersect1d(np.where(np.array(self.inside)==True), np.where(np.array(inside)==False))
-        delidx = np.where(np.array(self.inside)*(np.array(inside) == False))[0]
-        self.inside = inside
-
-        # Log flight statistics when for deleted aircraft
-        if len(delidx) > 0:
-            self.logger.log(
-                np.array(traf.id)[delidx],
-                self.create_time[delidx],
-                sim.simt - self.create_time[delidx],
-                self.distance2D[delidx],
-                self.distance3D[delidx],
-                self.work[delidx],
-                traf.lat[delidx],
-                traf.lon[delidx],
-                traf.alt[delidx],
-                traf.tas[delidx],
-                traf.vs[delidx],
-                traf.hdg[delidx],
-                # traf.ap.origlat[delidx],
-                # traf.ap.origlon[delidx],
-                # traf.ap.destlat[delidx],
-                # traf.ap.destlon[delidx],
-                traf.asas.active[delidx],
-                traf.pilot.alt[delidx],
-                traf.pilot.tas[delidx],
-                traf.pilot.vs[delidx],
-                traf.pilot.hdg[delidx]
-            )
-            # delete all aicraft in self.delidx
-            traf.delete(delidx)
-
-
-        # delete all aicraft in self.delidxalt
-        if len(delidxalt)>0:
-            traf.delete(list(delidxalt))
-
-    def set_area(self, *args):
+    def set_area(self, *args, exparea=False):
         ''' Set Experiment Area. Aicraft leaving the experiment area are deleted.
         Input can be exisiting shape name, or a box with optional altitude constrainsts.'''
-
+        curname = self.exparea if exparea else self.delarea
+        msgname = 'Experiment area' if exparea else 'Deletion area'
         # if all args are empty, then print out the current area status
         if not args:
-            return True, "Area is currently " + ("ON" if self.active else "OFF") + \
-                         "\nCurrent Area name is: " + str(self.name)
+            return True, f'{msgname} is currently ON (name={curname})' if self.active else \
+                         f'{msgname} is currently OFF'
 
         # start by checking if the first argument is a string -> then it is an area name
         if isinstance(args[0], str) and len(args)==1:
             if areafilter.hasArea(args[0]):
                 # switch on Area, set it to the shape name
-                self.name = args[0]
-                self.active = True
-                self.logger.start()
-                return True, "Area is set to " + str(self.name)
-            if args[0]=='OFF' or args[0]=='OF':
-                # switch off the area
-                areafilter.deleteArea(self.name)
-                self.logger.reset()
-                self.active = False
-                self.name = None
-                return True, "Area is switched OFF"
+                if exparea:
+                    self.exparea = args[0]
+                else:
+                    self.delarea = args[0]
 
+                self.active = True
+                self.flst.start()
+                self.conflog.start()
+                return True, f'{msgname} is set to {args[0]}'
+            if args[0][:2] =='OF':
+                # switch off the area and reset the logger
+                self.active = False
+                return True, f'{msgname} is switched OFF'
+            if args[0][:2] == 'ON':
+                if not self.name:
+                    return False, 'No area defined.'
+                else:
+                    self.active = True
+                    return True, f'{msgname} switched ON (name={curname})'
             # shape name is unknown
-            return False, "Shapename unknown. " + \
-                "Please create shapename first or shapename is misspelled!"
+            return False, 'Shapename unknown. ' + \
+                'Please create shapename first or shapename is misspelled!'
         # if first argument is a float -> then make a box with the arguments
         if isinstance(args[0],(float, int)) and 4<=len(args)<=6:
             self.active = True
-            self.name = 'DELAREA'
-            areafilter.defineArea(self.name, 'BOX', args[:4], *args[4:])
-            self.logger.start()
-            return True, "Area is ON. Area name is: " + str(self.name)
+            if exparea:
+                self.exparea = 'EXPAREA'
+                areafilter.defineArea('EXPAREA', 'BOX', args[:4], *args[4:])
+            else:
+                self.delarea = 'DELAREA'
+                areafilter.defineArea('DELAREA', 'BOX', args[:4], *args[4:])
+            self.flst.start()
+            self.conflog.start()
+            return True, f'{msgname} is ON. Area name is: {"EXP" if exparea else "DEL"}AREA'
 
-        return False,  "Incorrect arguments" + \
-                       "\nAREA Shapename/OFF or\n Area lat,lon,lat,lon,[top,bottom]"
+        return False,  'Incorrect arguments' + \
+                       '\nAREA Shapename/OFF or\n Area lat,lon,lat,lon,[top,bottom]'
 
     def set_taxi(self, flag,alt=1500*ft):
-        """ Taxi ON/OFF to autodelete below a certain altitude if taxi is off"""
+        ''' Taxi ON/OFF to autodelete below a certain altitude if taxi is off'''
         self.swtaxi = flag # True =  taxi allowed, False = autodelete below swtaxialt
         self.swtaxialt = alt
