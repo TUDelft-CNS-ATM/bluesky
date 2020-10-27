b69f5c865bc6b7d7616ff164257098e2d33c48f2
Joost Ellerbroek <jooste@users.noreply.github.com>
2020-10-27 10:05:49 +0100

    Update opensky.py

--- plugins/opensky.py
+++ plugins/opensky.py
@@ -18,7 +18,7 @@ import requests
 import numpy as np
 
 from bluesky import stack, settings, traf, scr
-from bluesky.core import TrafficArrays
+from bluesky.core import Entity, timed_function
 from bluesky.tools import cachefile
 settings.set_variable_defaults(opensky_user=None, opensky_password=None,
                                opensky_ownonly=False)
@@ -37,20 +37,10 @@ def init_plugin():
 
     config = {
         'plugin_name': 'OPENSKY',
-        'plugin_type': 'sim',
-        'update_interval': 6.0,
-        'preupdate': reader.update
+        'plugin_type': 'sim'
     }
 
-    stackfunctions = {
-        'OPENSKY': [
-            'OPENSKY [on/off]',
-            '[onoff]',
-            reader.toggle,
-            'Select OpenSky as a data source for traffic']
-    }
-
-    return config, stackfunctions
+    return config
 
 def get_actypedb():
     ''' Get aircraft type database from cache or web. '''
@@ -69,7 +59,7 @@ def get_actypedb():
             cache.dump(actypes)
 
 
-class OpenSkyListener(TrafficArrays):
+class OpenSkyListener(Entity):
     def __init__(self):
         super().__init__()
         if settings.opensky_user:
@@ -104,6 +94,7 @@ class OpenSkyListener(TrafficArrays):
             return list(zip(*states_json['states']))
         return None
 
+    @timed_function(name='OPENSKY', dt=6.0)
     def update(self):
         if not self.connected:
             return
@@ -181,11 +172,13 @@ class OpenSkyListener(TrafficArrays):
         # t5 = time.time()
         # print('req={}, mod={}, cre={}, mov={}, del={}, ncre={}, nmov={}, ndel={}'.format(curtime-t1, t2-curtime, t3-t2, t4-t3, t5-t4, n_new, n_oth, len(delidx)))
 
-    def toggle(self, flag=None):
+    @stack.command(name='OPENSKY')
+    def toggle(self, flag:bool=None):
+        ''' Use the OpenSky plugin as a data source for traffic. '''
+        if flag is None:
+            return True, f'OpenSky is currently {"" if self.connected else "not"} connected.'
+        self.connected = flag
         if flag:
-            self.connected = True
             stack.stack('OP')
             return True, 'Connecting to OpenSky'
-        else:
-            self.connected = False
-            return True, 'Stopping the requests'
+        return True, 'Stopping the requests'
