fc063f65656520e2d9c2a98ba34634537b69ce3e
Joost Ellerbroek <jooste@users.noreply.github.com>
2020-11-17 09:10:15 +0100

    Bugfix for batch mode

--- bluesky/stack/__init__.py
+++ bluesky/stack/__init__.py
@@ -3,7 +3,7 @@
     The stack parses all text-based commands in the simulation.
 '''
 from bluesky.stack.stack import stack, init, reset, process, sender, \
-    routetosender, get_scenname, get_scendata, set_scendata, ic
+    routetosender, get_scenname, get_scendata, set_scendata, ic, readscn
 from bluesky.stack.cmdparser import command, commandgroup, append_commands, \
     remove_commands, get_commands
 from bluesky.stack.argparser import refdata
