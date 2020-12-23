dbe83eeb5173ea749ddc4da965a8cd0c1f941004
Brover101 <mrassam258@gmail.com>
2020-12-23 23:09:26 +0500

    Waypoints added

--- plugins/simgen.py
+++ plugins/simgen.py
@@ -8,9 +8,9 @@ from bluesky.tools.aero import fpm, kts, ft, g0 #, Rearth, nm, tas2cas,\
 LGW_LAT = 51.1537
 LGW_LON = 0.1821
 
-SID = [(i + LGW_LAT, i + LGW_LON) for i in range(0, 10, 2)]
-STAR = [(LGW_LAT - i, LGW_LON - i) for i in range(10, -1, -2)]
-WPT_NAMES = ['WPT' + str(i) for i in range(0, len(SID))]
+SID = [i + LGW_LON for i in range(0, 10, 2)]
+STAR = [LGW_LON - i for i in range(10, -1, -2)]
+WPT_NAME = "ACORN"
 WPT_MAX = 4000
 WPT_MIN = 3000
 SPAWN_RADIUS = lambda : randint(0, 10)
@@ -43,6 +43,7 @@ class Simgen(Entity):
     SID = get_SID()
     STAR = get_STAR()
     sidstar = []
+    counter = 0
 
     def __init__(self):
         super.__init__(self)
@@ -84,55 +85,60 @@ class Simgen(Entity):
         
         #adding waypoints
 
-        for i in range(n):
-            # if self.sidstar[i] == 0: #departing
-            #     self.actwp.lat[i], self.actwp.lon[i] = self.SID[1]
-            # else:
-            #     self.actwp.lat[i], self.actwp.lon[i] = self.STAR[1]
-            acid = traf.id[i]
+        # for i in range(n):
+        #     # if self.sidstar[i] == 0: #departing
+        #     #     self.actwp.lat[i], self.actwp.lon[i] = self.SID[1]
+        #     # else:
+        #     #     self.actwp.lat[i], self.actwp.lon[i] = self.STAR[1]
+        #     acid = traf.id[i]
             
-            stack.stack(f'ECHO Adding initial waypoint {Simgen.SID[0] if Simgen.sidstar[i] == 0 else Simgen.STAR[0]}' +
-                        'to {acid}')
+        #     stack.stack(f'ECHO Adding initial waypoint {Simgen.SID[0] if Simgen.sidstar[i] == 0 else Simgen.STAR[0]}' +
+        #                 'to {acid}')
 
-            stack.stack(f'ADDWPT {acid}, {WPT_NAMES[0]}, {Simgen.SID[0][0] if Simgen.sidstar[i] == 0 else Simgen.STAR[0][0]},' + 
-                        f'{Simgen.SID[0][1] if Simgen.sidstar[i] == 1 else Simgen.STAR[0][1]}, FLYBY, {traf.alt[i] + randint(0, 100)}, ' +
-                        f'{traf.tas[i] + randint(-100, 100)}')
+        #     stack.stack(f'ADDWPT {acid}, {WPT_NAMES[0]}, {Simgen.SID[0][0] if Simgen.sidstar[i] == 0 else Simgen.STAR[0][0]},' + 
+        #                 f'{Simgen.SID[0][1] if Simgen.sidstar[i] == 1 else Simgen.STAR[0][1]}, FLYBY, {traf.alt[i] + randint(0, 100)}, ' +
+        #                 f'{traf.tas[i] + randint(-100, 100)}')
 
     @classmethod
     @timed_function(dt=5)
     def update(cls):
         ''' Periodic update function for our example entity. '''
-        
         n = randint(1, 4)
-        cls.nextwpt.extend([1 for _ in range(n)])
-        stack.stack(f"MCRE {n}")
-        Simgen.sidstar.extend([randint(0, 1) for _ in range(n)])
+        if cls.counter < 5:
+            stack.stack(f"MCRE {n}")
+            Simgen.sidstar.extend([randint(0, 1) for _ in range(n)])
+        
         #self.create(n = randint(1, 4))
-        for i in range(traf.ntraf):
-            #get current waypoint position and next waypoint index
-            #currwpt = self.actwp.lat[i], self.actwp.lon[i]
-            nextwpt = cls.nextwpt[i]
-            
-            if (nextwpt):
+        if cls.counter < len(SID):
+            for i in range(traf.ntraf):
+                #get current waypoint position and next waypoint index
+                #currwpt = self.actwp.lat[i], self.actwp.lon[i]            
+                for j in range(len(SID)):
+                    
+                    #nextwpt = cls.nextwpt[i]
+                    acid = traf.id[i]
+                    
+                    if cls.counter == 1:
+                        stack.stack(f'DEST {acid}, EGKK')
 
-                acid = traf.id[i]
+                    stack.stack(f'ECHO Adding next waypoint {cls.SID[j] if cls.sidstar[i] == 0 else cls.STAR[j]} called' +
+                                f'{WPT_NAME, cls.SID[j]} to {acid}')
+                    if j:            
+                        stack.stack(f'{acid} AFTER {WPT_NAME} ADDWPT {WPT_NAME},' + 
+                                    f'{cls.SID[j] if cls.sidstar[i] == 0 else cls.STAR[j]}')
+                    else:
+                        stack.stack(f'ADDWPT {acid}, {WPT_NAME},' + 
+                                    f'{cls.SID[j] if cls.sidstar[i] == 0 else cls.STAR[j]}')
 
-                stack.stack(f'ECHO Adding next waypoint {cls.SID[nextwpt] if cls.sidstar[i] == 0 else cls.STAR[nextwpt]} called' +
-                            f' {WPT_NAMES[nextwpt]} to {acid}')
-                
-                stack.stack(f'{acid} AFTER {WPT_NAMES[nextwpt - 1]} ADDWPT {WPT_NAMES[nextwpt]},' + 
-                            f'{cls.SID[nextwpt][0] if cls.sidstar[i] == 0 else cls.STAR[nextwpt][0]}, ' + 
-                            f'{cls.SID[nextwpt][1] if cls.sidstar[i] == 0 else cls.STAR[nextwpt][1]},' + 
-                            f'{traf.tas[i] + randint(-100, 100)}, {traf.alt[i] + randint(-100, 100)}')
+        cls.counter += 1
+            # else:
 
-            else:
+            #     stack.stack(f'ECHO Adding initial waypoint WPT0 to {acid}')
+            #     stack.stack(f'ADDPT {acid}, WPT0, ' +
+            #                 f'{cls.SID[nextwpt][0] if cls.sidstar[i] == 0 else cls.STAR[nextwpt][0]}, ' + 
+            #                 f'{cls.SID[nextwpt][1] if cls.sidstar[i] == 0 else cls.STAR[nextwpt][1]},' + 
+            #                 f'{traf.tas[i] + randint(-100, 100)}, {traf.alt[i] + randint(-100, 100)}')
 
-                stack.stack(f'ECHO Adding initial waypoint WPT0 to {acid}')
-                stack.stack(f'ADDPT {acid}, WPT0, ' +
-                            f'{cls.SID[nextwpt][0] if cls.sidstar[i] == 0 else cls.STAR[nextwpt][0]}, ' + 
-                            f'{cls.SID[nextwpt][1] if cls.sidstar[i] == 0 else cls.STAR[nextwpt][1]},' + 
-                            f'{traf.tas[i] + randint(-100, 100)}, {traf.alt[i] + randint(-100, 100)}')
-
-            cls.nextwpt[i] = cls.nextwpt[i] + 1
+            #cls.nextwpt[i] = cls.nextwpt[i] + 1
                 
 
