f8c8c30e9b242e0668ea871ad9928f4b47bddf2a
Joost Ellerbroek <j.ellerbroek@gmail.com>
2021-05-12 19:33:26 +0200

    Create glpoly.py

---
+++ bluesky/ui/qtgl/glpoly.py
@@ -0,0 +1,127 @@
+''' BlueSky OpenGL line and polygon (areafilter) drawing. '''
+import numpy as np
+import bluesky as bs
+from bluesky.ui import palette
+from bluesky.ui.qtgl import console
+import bluesky.ui.qtgl.glhelpers as glh
+
+
+palette.set_default_colours(
+    polys=(0, 0, 255),
+    previewpoly=(0, 204, 255)
+) 
+
+# Static defines
+POLYPREV_SIZE = 100
+POLY_SIZE = 2000
+
+VERTEX_IS_LATLON, VERTEX_IS_METERS, VERTEX_IS_SCREEN = list(range(3))
+
+
+class Poly(glh.RenderObject):
+    ''' Poly OpenGL object. '''
+
+    def __init__(self, parent):
+        super().__init__(parent)
+        # Polygon preview object
+        self.polyprev = glh.VertexArrayObject(glh.gl.GL_LINE_LOOP)
+
+        # Fixed polygons
+        self.allpolys = glh.VertexArrayObject(glh.gl.GL_LINES)
+        self.allpfill = glh.VertexArrayObject(glh.gl.GL_TRIANGLES)
+
+        self.prevmousepos = (0, 0)
+
+        bs.Signal('cmdline_stacked').connect(self.cmdline_stacked)
+        bs.Signal('radarmouse').connect(self.previewpoly)
+        bs.net.actnodedata_changed.connect(self.actdata_changed)
+
+    def create(self):
+        self.polyprev.create(vertex=POLYPREV_SIZE * 8,
+                             color=palette.previewpoly, usage=glh.gl.GL_DYNAMIC_DRAW)
+        self.allpolys.create(vertex=POLY_SIZE * 16, color=POLY_SIZE * 8)
+        self.allpfill.create(vertex=POLY_SIZE * 24,
+                             color=np.append(palette.polys, 50))
+
+    def draw(self):
+        actdata = bs.net.get_nodedata()
+        # Send the (possibly) updated global uniforms to the buffer
+        self.shaderset.set_vertex_scale_type(VERTEX_IS_LATLON)
+
+        # --- DRAW THE MAP AND COASTLINES ---------------------------------------------
+        # Map and coastlines: don't wrap around in the shader
+        self.shaderset.enable_wrap(False)
+
+        # --- DRAW PREVIEW SHAPE (WHEN AVAILABLE) -----------------------------
+        self.polyprev.draw()
+
+        # --- DRAW CUSTOM SHAPES (WHEN AVAILABLE) -----------------------------
+        if actdata.show_poly > 0:
+            self.allpolys.draw()
+            if actdata.show_poly > 1:
+                self.allpfill.draw()
+        
+    def cmdline_stacked(self, cmd, args):
+        if cmd in ['AREA', 'BOX', 'POLY', 'POLYGON', 'CIRCLE', 'LINE', 'POLYLINE']:
+            self.polyprev.set_vertex_count(0)
+
+    # def previewpoly(self, shape_type, data_in=None):
+    def previewpoly(self, mouseevent):
+        if mouseevent.type() != mouseevent.MouseMove:
+            return
+        mousepos = (mouseevent.x(), mouseevent.y())
+        # Check if we are updating a preview poly
+        if mousepos != self.prevmousepos:
+            cmd = console.get_cmd()
+            nargs = len(console.get_args())
+            if cmd in ['AREA', 'BOX', 'POLY', 'POLYLINE',
+                        'POLYALT', 'POLYGON', 'CIRCLE', 'LINE'] and nargs >= 2:
+                self.prevmousepos = mousepos
+                try:
+                    # get the largest even number of points
+                    start = 0 if cmd == 'AREA' else 3 if cmd == 'POLYALT' else 1
+                    end = ((nargs - start) // 2) * 2 + start
+                    data = [float(v) for v in console.get_args()[start:end]]
+                    data += self.glsurface.pixelCoordsToLatLon(*mousepos)
+                    self.glsurface.makeCurrent()
+                    if cmd is None:
+                        self.polyprev.set_vertex_count(0)
+                        return
+                    if cmd in ['BOX', 'AREA']:
+                        # For a box (an area is a box) we need to add two additional corners
+                        polydata = np.zeros(8, dtype=np.float32)
+                        polydata[0:2] = data[0:2]
+                        polydata[2:4] = data[2], data[1]
+                        polydata[4:6] = data[2:4]
+                        polydata[6:8] = data[0], data[3]
+                    else:
+                        polydata = np.array(data, dtype=np.float32)
+
+                    if cmd[-4:] == 'LINE':
+                        self.polyprev.set_primitive_type(glh.gl.GL_LINE_STRIP)
+                    else:
+                        self.polyprev.set_primitive_type(glh.gl.GL_LINE_LOOP)
+
+                    self.polyprev.update(vertex=polydata)
+
+
+                except ValueError:
+                    pass
+
+    def actdata_changed(self, nodeid, nodedata, changed_elems):
+        ''' Update buffers when a different node is selected, or when
+            the data of the current node is updated. '''
+        # Shape data change
+        if 'SHAPE' in changed_elems:
+            if nodedata.polys:
+                self.glsurface.makeCurrent()
+                contours, fills, colors = zip(*nodedata.polys.values())
+                # Create contour buffer with color
+                self.allpolys.update(vertex=np.concatenate(contours),
+                                     color=np.concatenate(colors))
+
+                # Create fill buffer
+                self.allpfill.update(vertex=np.concatenate(fills))
+            else:
+                self.allpolys.set_vertex_count(0)
+                self.allpfill.set_vertex_count(0)
