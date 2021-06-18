0ec8b663b7833913ee381926ff77335c3ffd4b5f
Joost Ellerbroek <j.ellerbroek@gmail.com>
2021-06-18 17:23:45 +0200

    Create clientexample.py

---
+++ plugins/clientexample.py
@@ -0,0 +1,57 @@
+""" BlueSky plugin template. The text you put here will be visible
+    in BlueSky as the description of your plugin. """
+import numpy as np
+# Import the global bluesky objects. Uncomment the ones you need
+from bluesky import stack  #, settings, navdb, sim, scr, tools
+import bluesky.ui.qtgl.glhelpers as ui
+from bluesky.ui.qtgl.glhelpers import gl
+
+
+### Initialization function of your plugin. Do not change the name of this
+### function, as it is the way BlueSky recognises this file as a plugin.
+def init_plugin():
+    ''' Plugin initialisation function. '''
+    # Configuration parameters
+    config = {
+        # The name of your plugin
+        'plugin_name':     'CLIENTEXAMPLE',
+
+        # The type of this plugin.
+        'plugin_type':     'gui',
+        }
+
+    # init_plugin() should always return a configuration dict.
+    return config
+
+
+### Entities in BlueSky are objects that are created only once (called singleton)
+### which implement some traffic or other simulation functionality.
+### To define an entity that ADDS functionality to BlueSky, create a class that
+### inherits from bluesky.core.Entity.
+### To replace existing functionality in BlueSky, inherit from the class that
+### provides the original implementation (see for example the asas/eby plugin).
+class MyVisual(ui.RenderObject, layer=100):
+    ''' Example new render object for BlueSky. '''
+    def __init__(self, parent):
+        super().__init__(parent=parent)
+        self.shape = ui.VertexArrayObject(gl.GL_TRIANGLE_FAN)
+
+    def create(self):
+        vertices = np.array([52, 5, 52, 4, 54, 4, 54, 5], dtype = np.float32)
+        self.shape.create(vertex=vertices, color=(255, 0, 0))
+
+    def draw(self):
+        self.shaderset.set_vertex_scale_type(0)
+        self.shape.draw()
+
+    # You can create new stack commands with the stack.command decorator.
+    # By default, the stack command name is set to the function name.
+    # The default argument type is a case-sensitive word. You can indicate different
+    # types using argument annotations. This is done in the below function:
+    # - The acid argument is a BlueSky-specific argument with type 'acid'.
+    #       This converts callsign to the corresponding index in the traffic arrays.
+    # - The count argument is a regular int.
+    @stack.command
+    def mycolor(self, color: 'color'):
+        ''' Set the color of my example shape. '''
+        self.shape.single_color[1:4] = color
