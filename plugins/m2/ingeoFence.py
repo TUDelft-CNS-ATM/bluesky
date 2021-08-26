a0d44ea83545c506496e4b8bfe56f8afbe6a7f54
Vincent de Vries <vincent.de.vries@nlr.nl>
2021-08-26 14:43:49 +0200

    added some todo's, comments and changed the intersection to a multipoly so all the geofences are checked at once.

--- plugins/m2/ingeoFence.py
+++ plugins/m2/ingeoFence.py
@@ -2,13 +2,11 @@
     in BlueSky as the description of your plugin. """
 from random import randint
 import numpy as np
-from shapely.geometry import Polygon, MultiPolygon, LineString
-from bluesky import core, stack, traf, tools
+from shapely.geometry import Polygon, MultiPolygon
+from bluesky import core, stack, traf, tools, settings
 
-
-geofence_names = tools.areafilter.basic_shapes.keys()
 geofences = tools.areafilter.basic_shapes
-acintent = traf.intent
+geofence_names = geofences.keys()
 
 
 def init_plugin():
@@ -31,34 +29,51 @@ class ingeoFence(core.Entity):
         with self.settrafarrays():
             self.ingeofence = np.array([],dtype=bool)
 
-    @core.timed_function(name='ingeofence', dt=3)
+    @core.timed_function(name='ingeofence', dt=settings.asas_dt, hook='postupdate')
     def update(self):
+        #check for each aircraft if it interferes with one of the geofences
         for i in traf.id:
             idx=traf.id2idx(i)
             val = self.checker(acid=idx)
             self.ingeofence[idx] = val
 
     def checker(self, acid: 'acid'):
-        #list that shows true or false per geofence for this specific aircraft
-        infence=[]
-
+        multiGeofence = []
         # check for each geofence if the aircrafts intent intersects with the geofence
+        # TODO Check if we can only run below function if a new geofence gets created... sort of like the super.create
         for j in geofence_names:
 
             # restructure the coordinates of the BS Poly shape to cast it into a shapely Polygon
             coord_list = list(zip(geofences[j].coordinates[1::2],geofences[j].coordinates[0::2]))
 
-            #construct shapely Polygon object
+            #construct shapely Polygon object and add it to the multipolygon list
             shapely_geofence = Polygon(coord_list)
+            multiGeofence.append(shapely_geofence)
 
-            # get the aircraft intent to check against current geofence
+        # get the aircraft intent to check against current geofence
+        # at startup aircraft dont have intent yet, if so, return False
+        # TODO ask if there is a more elegant solution to this (instead of try and except).
+        try:
             acintent = traf.intent[acid]
-            val = acintent[0].intersects(shapely_geofence)
-            infence.append(val)
-
-        #check if the aircraft intersects with any of the geofences, if yes return True
-        if any(infence):
-            return True
-        #if not return False, also meant to reset old Trues back to False when neccessary
-        else:
+        except:
             return False
+
+        #construct the multipolygon object from all the polygons
+        #this way you only have to check each aircraft against one shapely object instead of when each geofence in its own.
+        multiGeofence = MultiPolygon(multiGeofence)
+
+        #check for intersect between acintent and multipolygon
+        val = acintent[0].intersects(multiGeofence)
+
+        return val
+
+    @stack.command
+    def echoacgeofence(self, acid: 'acid'):
+        ''' Print the if an acid is in conflict with a geofence '''
+        geofence = self.getacgeofence(acid)
+        return True, f'{traf.id[acid]} geofence conflict {geofence}.'
+
+    def getacgeofence(self, acid: 'acid'):
+        ''' return the bool value in ingeofence of a specific acid '''
+        val = self.ingeofence[acid]
+        return val
