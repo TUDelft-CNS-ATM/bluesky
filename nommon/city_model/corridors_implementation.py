211b33cdbcef5c06bd8d9e9b5aba8b1f346c5885
mbaena <mbaena@192.168.2.167>
2021-09-07 09:49:26 +0200

    Corridors automation.
    Corridors loaded from csv file defined in the configuration file.
    Corridors entry lane created.

--- nommon/city_model/corridors_implementation.py
+++ nommon/city_model/corridors_implementation.py
@@ -3,6 +3,12 @@
 """
 
 """
+from builtins import int
+from distutils.command.config import config
+import configparser
+import csv
+import math
+import os
 
 from dynamic_segments import defineSegment
 import osmnx as ox
@@ -13,25 +19,101 @@ __copyright__ = '(c) Nommon 2021'
 
 
 def insertionNode( G, lon, lat, altitude ):
-    nearest_node = ox.distance.nearest_nodes( G, X=lon, Y=lat )
+    # nearest_node = ox.distance.nearest_nodes( G, X=lon, Y=lat )
 
     nodes = list( G.nodes )
-
-    nearest_latlon = list( filter( lambda node: str( node )[1:] == nearest_node[1:], nodes ) )
+    # nearest_latlon = list( filter( lambda node: str( node )[1:] == nearest_node[1:]), nodes )
+    nearest_latlon = list( filter( lambda node: str( node )[:3] != 'COR' , nodes ) )
 
     nearest_node = nearest_latlon[0]
-    delta_z = abs( G.nodes[nearest_node]['z'] - altitude )
+    delta_z = abs( ( G.nodes[nearest_node]['z'] - altitude ) ** 2 +
+                  ( G.nodes[nearest_node]['y'] - lat ) ** 2 +
+                  ( G.nodes[nearest_node]['x'] - lon ) ** 2 )
 
     for node in nearest_latlon[1:]:
-        delta_z_aux = abs( G.nodes[node]['z'] - altitude )
+        delta_z_aux = abs( ( G.nodes[node]['z'] - altitude ) ** 2 +
+                  ( G.nodes[node]['y'] - lat ) ** 2 +
+                  ( G.nodes[node]['x'] - lon ) ** 2 )
         if delta_z_aux < delta_z:
             delta_z = delta_z_aux
             nearest_node = node
-
     return nearest_node
 
+def latLon2XY( lat, lon, lat_0 ):
+    '''
 
-def corridorCreation( G, segments, corridor_coordinates, altitude, speed, capacity, name ):
+    '''
+
+def entryNodes( G, segments, node, name, speed, next_node, config ):
+    n_layers = config['Layers'].getint( 'number_of_layers' )
+    layer_width = config['Layers'].getint( 'layer_width' )
+    entry_low_height = n_layers * layer_width
+    # Get longitude and latitude for the acceleration entry point
+    lon = G.nodes[node]['x']
+    lat = G.nodes[node]['y']
+    lon_next = G.nodes[next_node]['x']
+    lat_next = G.nodes[next_node]['y']
+
+    # transform latitude, longitude to x, y
+    r = 6378137
+    x = r * math.radians( lon ) * math.cos( math.radians( lat ) )
+    y = r * math.radians( lat )
+    x_next = r * math.radians( lon_next ) * math.cos( math.radians( lat ) )
+    y_next = r * math.radians( lat_next )
+
+    # Unit vector opposite to the next point
+    mod = math.sqrt( ( y_next - y ) ** 2 + ( x_next - x ) ** 2 )
+    unit_vector_x = ( x - x_next ) / mod
+    unit_vector_y = ( y - y_next ) / mod
+    entry_angle = math.atan2( unit_vector_y, unit_vector_x ) + math.pi / 6
+
+    # Entry point coordinates
+    acceleration_lenght = config['Corridors'].getint( 'acceleration_length' )
+    entry_x = x + math.cos( entry_angle ) * acceleration_lenght
+    entry_y = y + math.sin( entry_angle ) * acceleration_lenght
+    entry_lon = math.degrees( entry_x / ( r * math.cos( math.radians( lat ) ) ) )
+    entry_lat = math.degrees( entry_y / r )
+    # Adding nodes
+    node_low = node + '_entry_1'
+    node_high = node + '_entry_2'
+    G.addNodeAltitude( node_low, entry_lat, entry_lon, entry_low_height )
+    G.addNodeAltitude( node_high, entry_lat, entry_lon, G.nodes[node]['z'] )
+    # Adding edges for those nodes
+    G.add_edge( node_low, node_high, 0, oneway=False, segment='new',
+                    speed=speed, length=G.nodes[node]['z'] - entry_low_height )
+    G.add_edge( node_high, node_low, 0, oneway=False, segment=name,
+                    speed=speed, length=G.nodes[node]['z'] - entry_low_height )
+
+    G.add_edge( node_high, node, 0, oneway=False, segment='new', speed=speed,
+               length=ox.distance.great_circle_vec( entry_lat, entry_lon,
+                                                         G.nodes[node]['y'],
+                                                         G.nodes[node]['x'] ) )
+    G.add_edge( node, node_high, 0, oneway=False, segment=name, speed=speed,
+               length=ox.distance.great_circle_vec( entry_lat, entry_lon,
+                                                         G.nodes[node]['y'],
+                                                         G.nodes[node]['x'] ) )
+
+    # Linking the lower entry point with the city grid
+    node_G = insertionNode( G, entry_lon, entry_lat, entry_low_height )
+    # print( node_G )
+    delta_z = G.nodes[node_G]['z'] - G.nodes[node_low]['z']
+    # print( 'z grid node', G.nodes[node_G]['z'] )
+    # print( 'z entry node', G.nodes[node_low]['z'] )
+
+    delta_xy = ox.distance.great_circle_vec( G.nodes[node_G]['y'], G.nodes[node_G]['x'],
+                                                 G.nodes[node_low]['y'],
+                                                 G.nodes[node_low]['x'] )
+
+    length = ( delta_z ** 2 + delta_xy ** 2 ) ** ( 1 / 2 )
+
+    G.add_edge( node_G, node_low, 0, oneway=False, segment='new', speed=50.0,
+                    length=length )
+    G.add_edge( node_low, node_G, 0, oneway=False, segment=name, speed=speed,
+                    length=length )
+
+    return G, segments
+
+def corridorCreation( G, segments, corridor_coordinates, altitude, speed, capacity, name, config ):
     segments = defineSegment( segments, 0, 0, 0, 0, 0, 0, speed, capacity, name )
     segments[name]['updated'] = False
     segments[name]['new'] = False
@@ -45,34 +127,104 @@ def corridorCreation( G, segments, corridor_coordinates, altitude, speed, capaci
         point_lon = point[0]
         point_lat = point[1]
 
-        nodes_G += [insertionNode( G, point_lon, point_lat, altitude )]
+                # nodes_G += [insertionNode( G, point_lon, point_lat, altitude )]
 
         G.addNodeAltitude( nodes_corridor[-1], point_lat, point_lon, altitude )
 
+        # Adds corridor edges to G
         if len( nodes_corridor ) == 1:
             continue
 
-        G.add_edge( nodes_corridor[-2], nodes_corridor[-1], 0, oneway=False, segment=name,
+        G.add_edge( nodes_corridor[-2], nodes_corridor[-1], 0, oneway=True, segment=name,
                     speed=speed,
                     length=ox.distance.great_circle_vec( point_lat, point_lon,
                                                          G.nodes[nodes_corridor[-2]]['y'],
                                                          G.nodes[nodes_corridor[-2]]['x'] ) )
 
-    for entry_node, corridor_point in zip( nodes_G, nodes_corridor ):
-        delta_z = G.nodes[entry_node]['z'] - G.nodes[corridor_point]['z']
-        delta_xy = ox.distance.great_circle_vec( G.nodes[entry_node]['y'], G.nodes[entry_node]['x'],
-                                                 G.nodes[corridor_point]['y'],
-                                                 G.nodes[corridor_point]['x'] )
 
-        length = ( delta_z ** 2 + delta_xy ** 2 ) ** ( 1 / 2 )
+    # for entry_node, corridor_point in zip( nodes_G, nodes_corridor ):
+    #    delta_z = G.nodes[entry_node]['z'] - G.nodes[corridor_point]['z']
+    #    delta_xy = ox.distance.great_circle_vec( G.nodes[entry_node]['y'], G.nodes[entry_node]['x'],
+    #                                             G.nodes[corridor_point]['y'],
+    #                                             G.nodes[corridor_point]['x'] )
 
-        G.add_edge( entry_node, corridor_point, 0, oneway=False, segment='new', speed=50.0,
-                    length=length )
-        G.add_edge( corridor_point, entry_node, 0, oneway=False, segment=name, speed=speed,
-                    length=length )
+    #    length = ( delta_z ** 2 + delta_xy ** 2 ) ** ( 1 / 2 )
 
+    #    G.add_edge( entry_node, corridor_point, 0, oneway=False, segment='new', speed=50.0,
+    #                length=length )
+    #    G.add_edge( corridor_point, entry_node, 0, oneway=False, segment=name, speed=speed,
+    #                length=length )
+
+        # create entry points
+        entryNodes( G, segments, nodes_corridor[-2], name, speed, nodes_corridor[-1], config )
+
+    return G, segments
+
+def str2intList( string ):
+    '''
+    This function transforms a string containing digits and other characters into a list of integers
+
+    Input:
+        string: any string, e.g., '1, 2, 40,,w'
+    Output:
+        list: list with the integers contained in the string, e.g., ['1', '2', '40']
+    '''
+    list = []
+    for s in string.split():
+        if s.isdigit():
+             list.append( s )
+    return list
+
+def getCorridorCoordinates( corridor, file_path ):
+    '''
+    This function gets coordinates stored in a txt, csv or excel file
+
+    Input:
+        file_path: path to the file storing the coordinates and the corridors associated
+        corridor: number of the corridor required
+    Output:
+        coordinates: a tuple containing lists of [longitude, latitude] for
+            the points defining the corridor
+    '''
+    with open( file_path, 'r' ) as csv_file:
+        reader = csv.reader( csv_file, delimiter=';' )
+        # fields = reader.next()
+        rows = []
+        corridor_row = []
+        for row in reader:
+            rows.append( row )
+            if row[0] == corridor:
+                corridor_row.append( [float( row[2] ), float( row[1] )] )
+
+    return tuple( corridor_row )
+
+
+def corridorLoad( G, segments, config ):
+    active_corridors = str2intList( config['Corridors']['corridors'] )
+    file_path_corridors = config['Corridors']['file_path_corridors']
+    altitude = config['Corridors'].getint( 'altitude' )
+    delta_z = config['Corridors'].getint( 'delta_z' )
+    speed = config['Corridors'].getint( 'speed' )
+
+    for corridor in active_corridors:
+        # Creates a unique name for the corridor
+        name = 'COR_' + corridor
+        name_rev = 'COR_r_'
+        # Get corridor coordinates
+        corridor_coordinates = getCorridorCoordinates( corridor, file_path_corridors )
+        # Creates the segments of the corridor
+        G, segments = corridorCreation( G, segments, corridor_coordinates,
+                                                      altitude, speed, 50, name, config )
+        G, segments = corridorCreation( G, segments, corridor_coordinates[::-1],
+                                                      altitude + delta_z, speed, 50, name_rev,
+                                                      config )
     return G, segments
 
 
 if __name__ == '__main__':
-    pass
+    # CONFIG
+    config = configparser.ConfigParser()
+    config_path = "C:/workspace3/bluesky/nommon/city_model/settings.cfg"
+    config.read( config_path )
+
+    corridorLoad( config )
--- nommon/city_model/main.py
+++ nommon/city_model/main.py
@@ -3,15 +3,16 @@
 """
 
 """
-import configparser
 import time
 
-from nommon.city_model.auxiliar import read_my_graphml
-from nommon.city_model.city_graph import cityGraph
-from nommon.city_model.corridors_implementation import corridorCreation
-from nommon.city_model.dynamic_segments import dynamicSegments
-from nommon.city_model.multi_di_graph_3D import MultiDiGrpah3D
-from nommon.city_model.path_planning import trajectoryCalculation, printRoute
+import configparser
+
+from auxiliar import read_my_graphml
+from city_graph import cityGraph
+from corridors_implementation import corridorCreation, corridorLoad
+from dynamic_segments import dynamicSegments
+from multi_di_graph_3D import MultiDiGrpah3D
+from path_planning import trajectoryCalculation, printRoute
 import numpy as np
 import osmnx as ox
 
@@ -27,6 +28,8 @@ if __name__ == '__main__':
     config_path = "C:/workspace3/bluesky/nommon/city_model/settings.cfg"
     config.read( config_path )
 
+
+
     # City
     if config['City'].getboolean( 'import' ):
         filepath = r"C:\workspace3\bluesky\nommon\city_model\data\hannover_segments.graphml"
@@ -37,22 +40,24 @@ if __name__ == '__main__':
     else:
         G = cityGraph( config )
 
-#     fig, ax = ox.plot_graph( G )
+#    fig, ax = ox.plot_graph( G )
     # Segments
-#     G, segments = dynamicSegments( G, config, segments=None )
+#    G, segments = dynamicSegments( G, config, segments=None )
+#
+#    segments[list( segments.keys() )[0]]['new'] = True
+#    segments[list( segments.keys() )[3]]['updated'] = True
+#    segments[list( segments.keys() )[3]]['speed'] = 0
+#    G, segments = dynamicSegments( G, config, segments )
 
-#     segments[list( segments.keys() )[0]]['new'] = True
-#     segments[list( segments.keys() )[3]]['updated'] = True
-#     segments[list( segments.keys() )[3]]['speed'] = 0
-#     G, segments = dynamicSegments( G, config, segments )
-
-#     print( 'Saving the graph...' )
-#     filepath = "./data/hannover_segments.graphml"
-#     ox.save_graphml( G, filepath )
-#     np.save( 'my_segments.npy', segments )
+#    print( 'Saving the graph...' )
+#    filepath = "./data/hannover_segments_2.graphml"
+#    ox.save_graphml( G, filepath )
+#    np.save( 'my_segments_2.npy', segments )
 
     # Corridors
-#     G, segments = corridorCreation( G, segments, ( [9.73, 52.375], [9.77, 52.399 ] ),
+    G, segments = corridorLoad( G, segments, config )
+
+    # G, segments = corridorCreation( G, segments, ( [9.63055, 52.42053], [9.6478, 52.35128 ], [9.86469, 52.35893], [9.89988, 52.39687], [9.74728, 52.4264] ),
 #                                     200, 100, 50, 'COR_1' )
 #     G, segments = dynamicSegments( G, config, segments=None )
     # No-fly zones
@@ -64,20 +69,20 @@ if __name__ == '__main__':
     segments['segment_2_2_0']['updated'] = True
     segments['segment_2_2_1']['speed'] = 0
     segments['segment_2_2_1']['updated'] = True
-    segments['segment_1_2_0']['speed'] = 0
-    segments['segment_1_2_0']['updated'] = True
-    segments['segment_1_2_1']['speed'] = 0
-    segments['segment_1_2_1']['updated'] = True
-    segments['segment_1_3_0']['speed'] = 0
-    segments['segment_1_3_0']['updated'] = True
-    segments['segment_1_3_1']['speed'] = 0
-    segments['segment_1_3_1']['updated'] = True
-#     segments['segment_2_3_0']['speed'] = 0
-#     segments['segment_2_3_0']['updated'] = True
-#     segments['segment_2_3_1']['speed'] = 0
-#     segments['segment_2_3_1']['updated'] = True
-#     segments['segment_3_2_0']['speed'] = 0
-#     segments['segment_3_2_0']['updated'] = True
+#    segments['segment_1_2_0']['speed'] = 0
+#    segments['segment_1_2_0']['updated'] = True
+#    segments['segment_1_2_1']['speed'] = 0
+#    segments['segment_1_2_1']['updated'] = True
+#    segments['segment_1_3_0']['speed'] = 0
+#    segments['segment_1_3_0']['updated'] = True
+#    segments['segment_1_3_1']['speed'] = 0
+#    segments['segment_1_3_1']['updated'] = True
+#    segments['segment_2_3_0']['speed'] = 0
+#    segments['segment_2_3_0']['updated'] = True
+#    segments['segment_2_3_1']['speed'] = 0
+#    segments['segment_2_3_1']['updated'] = True
+#    segments['segment_3_2_0']['speed'] = 0
+#    segments['segment_3_2_0']['updated'] = True
 #     segments['segment_3_2_1']['speed'] = 0
 #     segments['segment_3_2_1']['updated'] = True
 #     segments['segment_3_3_0']['speed'] = 0
@@ -87,16 +92,28 @@ if __name__ == '__main__':
 #
     G, segments = dynamicSegments( G, config, segments )
 #     edges = ox.utils_graph.graph_to_gdfs( G, nodes=False, fill_edge_geometry=False )
-
     # Plot graph
-    ec = ["r" if G.edges[( u, v, k )]['speed'] == 0 else "gray" for u, v, k in G.edges( keys=True )]
+    # ec = ["r" if G.edges[( u, v, k )]['speed'] == 0
+    #      "b" if G.edges[( u, v, k )]['speed'] == 100
+    #      else "gray"
+    #      for u, v, k in G.edges( keys=True )]
+
+    ec = []
+    for u, v, k in G.edges( keys=True ):
+        if G.edges[( u, v, k )]['speed'] == 0:
+            ec.append( "r" )
+        elif G.edges[( u, v, k )]['speed'] == 100:
+            ec.append( "g" )
+        else:
+            ec.append( "gray" )
     fig, ax = ox.plot_graph( G, node_color="w", node_edgecolor="k", edge_color=ec,
                              edge_linewidth=2 )
 
     # Route
-    orig = [9.73, 52.38]
-    dest = [9.77, 52.39 ]
+    orig = [9.72321, 52.3761]
+    dest = [9.75413, 52.3554 ]
     length, route = trajectoryCalculation( G, orig, dest )
+
     print( length )
     printRoute( G, route )
 
