d1e9c4bb50764963b810ed77e73b7b5a734c5578
Joost Ellerbroek <jooste@users.noreply.github.com>
2022-01-17 13:44:21 +0100

    Fix to make pygame version runnable again

--- bluesky/ui/__init__.py
+++ bluesky/ui/__init__.py
@@ -1,4 +1,4 @@
 ''' BlueSky UI classes and functions. '''
-from bluesky.ui.qtgl.glhelpers import (gl, RenderObject, RenderTarget, RenderWidget,
-    ShaderProgram, ShaderSet, VertexArrayObject, GLBuffer, UniformBufferObject, Font,
-    Text, Texture, Circle, Rectangle)
+# from bluesky.ui.qtgl.glhelpers import (gl, RenderObject, RenderTarget, RenderWidget,
+#     ShaderProgram, ShaderSet, VertexArrayObject, GLBuffer, UniformBufferObject, Font,
+#     Text, Texture, Circle, Rectangle)
