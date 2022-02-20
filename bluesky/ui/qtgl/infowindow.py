8aee6eb95fd78a7dea2ac6678a2c08d1f6dbf7a5
Andres Morfin Veytia <78442543+amorfinv@users.noreply.github.com>
2022-02-20 12:11:05 +0100

    include pyqt5 imports in qtgl

--- bluesky/ui/qtgl/infowindow.py
+++ bluesky/ui/qtgl/infowindow.py
@@ -5,14 +5,20 @@ except ImportError:
     from collections import Collection
 import numpy as np
 import matplotlib
-matplotlib.use('QtAgg')
 matplotlib.rcParams['font.size'] = 5
 
 from matplotlib.figure import Figure
 from matplotlib.backend_bases import key_press_handler
 import matplotlib.pyplot as plt
-from PyQt6.QtCore import Qt
-from PyQt6.QtWidgets import QTabWidget, QVBoxLayout, QScrollArea, QWidget
+try:
+    from PyQt5.QtCore import Qt
+    from PyQt5.QtWidgets import QTabWidget, QVBoxLayout, QScrollArea, QWidget
+    matplotlib.use('Qt5Agg')
+except ImportError:
+    from PyQt6.QtCore import Qt
+    from PyQt6.QtWidgets import QTabWidget, QVBoxLayout, QScrollArea, QWidget
+    matplotlib.use('QtAgg')
+
 from matplotlib.backends.backend_qtagg import FigureCanvasQTAgg as FigureCanvas, \
     NavigationToolbar2QT as NavigationToolbar
 
--- bluesky/ui/qtgl/mainwindow.py
+++ bluesky/ui/qtgl/mainwindow.py
@@ -2,13 +2,22 @@
 import platform
 import os
 
-from PyQt6.QtWidgets import QApplication as app
-from PyQt6.QtCore import Qt, pyqtSlot, QTimer, QItemSelectionModel, QSize
-from PyQt6.QtGui import QPixmap, QIcon
-from PyQt6.QtWidgets import QMainWindow, QSplashScreen, QTreeWidgetItem, \
-    QPushButton, QFileDialog, QDialog, QTreeWidget, QVBoxLayout, \
-    QDialogButtonBox
-from PyQt6 import uic
+try:
+    from PyQt5.QtWidgets import QApplication as app
+    from PyQt5.QtCore import Qt, pyqtSlot, QTimer, QItemSelectionModel, QSize
+    from PyQt5.QtGui import QPixmap, QIcon
+    from PyQt5.QtWidgets import QMainWindow, QSplashScreen, QTreeWidgetItem, \
+        QPushButton, QFileDialog, QDialog, QTreeWidget, QVBoxLayout, \
+        QDialogButtonBox
+    from PyQt5 import uic
+except ImportError:
+    from PyQt6.QtWidgets import QApplication as app
+    from PyQt6.QtCore import Qt, pyqtSlot, QTimer, QItemSelectionModel, QSize
+    from PyQt6.QtGui import QPixmap, QIcon
+    from PyQt6.QtWidgets import QMainWindow, QSplashScreen, QTreeWidgetItem, \
+        QPushButton, QFileDialog, QDialog, QTreeWidget, QVBoxLayout, \
+        QDialogButtonBox
+    from PyQt6 import uic
 
 # Local imports
 import bluesky as bs
