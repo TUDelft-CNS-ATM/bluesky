c5d85517057f9debcd28407ddc615965e06dd0e1
Andres Morfin Veytia <78442543+amorfinv@users.noreply.github.com>
2022-02-20 14:56:47 +0100

    add traffic table

--- bluesky/ui/console/consoleui.py
+++ bluesky/ui/console/consoleui.py
@@ -1,3 +1,4 @@
+from operator import ge
 import string
 from typing import Union
 from textual import events
@@ -33,6 +34,7 @@ class ConsoleClient(Client):
     def __init__(self, actnode_topics=b''):
         super().__init__(actnode_topics)
         self.subscribe(b'SIMINFO')
+        self.subscribe(b'ACDATA')
         
         self.count = 0
         self.nodes = dict()
@@ -50,6 +52,7 @@ class ConsoleClient(Client):
         pass
 
     def stream(self, name, data, sender_id):
+
         if name == b'SIMINFO' and ConsoleUI.instance is not None:
             speed, simdt, simt, simutc, ntraf, state, scenname = data
             simt = tim2txt(simt)[:-3]
@@ -61,7 +64,40 @@ class ConsoleClient(Client):
                 #                           % (simt, simdt, speed, simutc, self.modes[state], ntraf, acdata.nconf_cur, acdata.nconf_tot, acdata.nlos_cur, acdata.nlos_tot))
 
             ConsoleUI.instance.set_nodes(copy.deepcopy(self.nodes))
-            
+        
+        if name == b'ACDATA':
+            if sender_id == bs.net.actnode():
+                gen_data, table_data = self.get_traffic(data)
+                ConsoleUI.instance.set_traffic(gen_data, table_data)
+
+    def get_traffic(self, data):
+        
+        # general data
+        gen_data = dict()
+        gen_data['simt'] = data['simt'] 
+        gen_data['nconf_cur'] = data['nconf_cur'] 
+        gen_data['nconf_tot'] = data['nconf_tot']
+        gen_data['nlos_cur'] = data['nlos_cur']
+        gen_data['nlos_tot'] = data['nlos_tot']
+        
+        # only keep some info for table
+        table_data = dict()
+
+        table_data['id'] = data['id']
+        table_data['lat'] = data['lat']
+        table_data['lon'] = data['lon']
+        table_data['alt'] = data['alt']
+        table_data['tas'] = data['tas']
+        table_data['cas'] = data['cas']
+        table_data['inconf'] = data['inconf']
+        table_data['tcpamax'] = data['tcpamax']
+        table_data['rpz'] = data['rpz']
+        table_data['vs'] = data['vs']
+        table_data['vmin'] = data['vmin']
+        table_data['vmax'] = data['vmax']
+
+        return gen_data, table_data
+        
     def setNodeInfo(self, connid, time, scenname):
         
         # check if it is inside self.nodes
@@ -128,23 +164,58 @@ class NodeInfo(Widget):
         
         self.nodepanel = Panel(self.table)
 
+class Traffic(Widget):
+    trafficpanel: Union[Reactive[Panel], Panel] = Reactive(Panel(Table()))
+
+    def __init__(self, text: str = "", name: str | None = None) -> None:
+        super().__init__(name)
+        self.text = text
+        self.table = Table()
+        self.trafficpanel = Panel(self.table)
+
+    def render(self) -> RenderableType:
+        return self.trafficpanel
+
+    def set_traffic(self, trafict: dict) -> None:
+        # add rows to table
+        self.table = Table()
+
+        # add the columns
+        for col in trafict.keys():
+            self.table.add_column(col)
+        
+        ntraf = len(trafict['id'])
+
+        if ntraf > 0:
+            for i in range(ntraf):
+                row = []
+                for col in trafict.keys():
+                    row.append(str(trafict[col][i]))
+                self.table.add_row(*row)
+        # # add the rows loop through ntraf
+        # for _, node in trafict.items():
+        #     self.table.add_row(node['num'], f'{node_id}', node['scenename'], node['time'])
+        
+        self.trafficpanel = Panel(self.table)
 
 class ConsoleUI(App):
     cmdtext: Union[Reactive[str], str] = Reactive("")
     echotext: Union[Reactive[str], str] = Reactive("")
     infotext: Union[Reactive[str], str] = Reactive("")
     nodedict = Reactive(dict())
+    trafsimt = Reactive("")
 
     cmdbox: Textline
     echobox: Echobox
     infoline: Textline
     nodeinfo: NodeInfo
+    traffic: Traffic
     instance: App
-
+    
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         ConsoleUI.instance = self
-
+        
     def echo(self, text, flags=None):
         # if flags != bs.BS_OK:
         #     text = f'[red]{text}[/red]'
@@ -156,6 +227,10 @@ class ConsoleUI(App):
     def set_nodes(self, nodes):
         self.nodedict = nodes
 
+    def set_traffic(self, gen_data, traffic):
+        self.trafdict = traffic
+        self.trafsimt = str(gen_data['simt'])
+        
     async def on_key(self, key: events.Key) -> None:
         if key.key == Keys.ControlH:
             self.cmdtext = self.cmdtext[:-1]
@@ -182,11 +257,15 @@ class ConsoleUI(App):
     async def watch_echotext(self, echotext) -> None:
         self.echobox.set_text(echotext)
 
+    async def watch_trafsimt(self, trafsimt) -> None:
+        self.traffic.set_traffic(self.trafdict)
+
     async def on_mount(self, event: events.Mount) -> None:
         self.cmdbox = Textline("[blue]>>[/blue]")
         self.echobox = Echobox(Panel(Text(), height=8, box=box.SIMPLE, style=Style(bgcolor="grey53")))
         self.infoline = Textline("[black]Current node: [/black]")
         self.nodeinfo = NodeInfo()
+        self.traffic = Traffic()
         
         await self.view.dock(self.cmdbox, edge="bottom", size=1)
         # await self.view.dock(self.echobox, edge="bottom", size=8)
@@ -198,7 +277,7 @@ class ConsoleUI(App):
         # await self.view.dock(Placeholder(), edge="right", size=20)
         await self.view.dock(self.infoline, edge="bottom", size=1)
 
-        await self.view.dock(self.nodeinfo, edge="top")
+        await self.view.dock(self.traffic, edge="top")
         await self.set_focus(self.cmdbox)
 
         self.set_interval(0.2, bs.net.update, name='Network')
