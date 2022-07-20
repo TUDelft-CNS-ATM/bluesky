e591ce7796fcaa7f7e0f80f1728d2ae6c04b1d8e
Andres Morfin Veytia <78442543+amorfinv@users.noreply.github.com>
2022-07-20 11:04:54 +0200

    update df traff array

--- plugins/dftraffarray.py
+++ plugins/dftraffarray.py
@@ -1,15 +1,16 @@
 import numpy as np
 import pandas as pd
 import geopandas as gpd
+from datetime import datetime, timedelta
 
-from bluesky import core 
+from bluesky import core, sim
 
 def init_plugin():
     ''' Plugin initialisation function. '''
     example = DF_arrays()
 
     config = {
-        'plugin_name':     'DFFUN',
+        'plugin_name':     'DFEXAMPLE',
         'plugin_type':     'sim',
         }
 
@@ -22,21 +23,32 @@ class DF_arrays(core.Entity):
         super().__init__()
 
         with self.settrafarrays():
-            self.passenger_data = pd.DataFrame({
-                                            'fare': pd.Series(dtype=float),
-                                            'age':  pd.Series(dtype=int),
-                                            'connecting': pd.Series(dtype=bool)
-                                            })
-
-            self.geom_fun = gpd.GeoDataFrame({
-                                            'a': pd.Series(dtype='int'),
-                                            'geometry': gpd.GeoSeries(dtype=object),
-                                            'c': pd.Series(dtype='float')
+            self.flight_data = pd.DataFrame({
+                                            'npassengers': pd.Series(dtype=int),
+                                            'mean_fare': pd.Series(dtype=float),
+                                            'departure_time': pd.Series(dtype=datetime),
+                                            'arrival_time': pd.Series(dtype=datetime),
+                                            'predicted_arrival_time': pd.Series(dtype=datetime),
+                                            'flight_distance': pd.Series(dtype=float),
+                                            'predicted_flight_distance': pd.Series(dtype=float),
                                             })
 
     def create(self, n=1):
         super().create(n)
 
-        self.passenger_data.loc[-n:, 'fare'] = 100
-        self.passenger_data.loc[-n:, 'age'] = 20
-        self.passenger_data.loc[-n:, 'connecting'] = True
+        # Get some data at start of simulation
+        self.flight_data.loc[-n:, 'npassengers'] = 10
+        self.flight_data.loc[-n:, 'mean_fare'] = 100
+        self.flight_data.loc[-n:, 'departure_time'] = datetime(2022, 1, 1) + timedelta(seconds = sim.simt)
+        self.flight_data.loc[-n:, 'predicted_arrival_time'] = datetime(2022, 1, 1) + timedelta(seconds = sim.simt) + timedelta(seconds=randint(0, 100))
+        self.flight_data.loc[-n:, 'predicted_flight_distance'] = randint(0, 100)
+
+    
+    def delete(self, idx):
+
+        # Get arrival time and flight distance
+        self.flight_data.loc[idx, 'arrival_time'] = self.flight_data.loc[idx, 'departure_time'] + timedelta(seconds = sim.simt)
+        self.flight_data.loc[idx, 'flight_distance'] = traf.distflown
+
+        # Call the actual delete function
+        super().delete(idx)
