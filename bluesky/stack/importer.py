f504f9c8d24540679d7167ef3343b01e70549b38
Joost Ellerbroek <jooste@users.noreply.github.com>
2022-08-24 16:00:08 +0200

    Moved filetype to subcommand, added possibility to add wildcards

--- bluesky/stack/importer.py
+++ bluesky/stack/importer.py
@@ -1,17 +1,17 @@
 from pathlib import Path
 from bluesky.core import Entity
-from bluesky.stack import command
+from bluesky.stack import commandgroup
 from bluesky.stack.simstack import process, merge
 
 class Importer(Entity):
-    @command(name='IMPORT')
+    @commandgroup(name='IMPORT')
     @staticmethod
-    def importcmd(fname:str='', ftype:str=''):
+    def importcmd(fname:str='', *args):
         ''' Importer: file importer for 3rd party scenario data.
         
             Arguments:
             - fname: The filename of the file to import
-            - ftype: The type of the file (optional)
+            - args: Optional arguments to perform e.g., wildcard replacement
         '''
         if not fname:
             ret = Importer.importcmd.__doc__
@@ -22,40 +22,38 @@ class Importer(Entity):
                     ret += f'\n{importer.filetype} (extensions: {", ".join(importer.extensions)})'
             return True, ret
         # Get the correct importer implementation
-        if ftype:
-            impl = Importer.derived().get(ftype)
-            if impl is None:
-                return False, f"No importer found for file type {ftype}"
-            importer = impl.implinstance()
+        ext = Path(fname).suffix.strip('.')
+        for impl in Importer.derived().values():
+            if impl is not Importer and ext in impl.implinstance().extensions:
+                importer = impl.implinstance()
+                break
         else:
-            ext = Path(fname).suffix.strip('.')
-            for impl in Importer.derived().values():
-                if impl is not Importer and ext in impl.implinstance().extensions:
-                    importer = impl.implinstance()
-                    break
-            else:
-                return False, f"No importer found for file with extension {ext}"
+            return False, f"No importer found for file with extension {ext}"
 
-        # Load the data, and merge with current scenario stack
-        scentime, scencmd = importer.load(fname)
-        # If there are no timestamps, process immediately
-        if not scentime:
-            process(zip(scencmd, len(scencmd) * [None]))
-            return True
-        # If we have timestamps, merge the commands with the existing scenario stack
-        merge(zip(scentime, scencmd))
-        return True
+        return importer._load_and_process(fname)
 
     def __init__(self, filetype, extensions=None):
         super().__init__()
         self.filetype = filetype
         self.extensions = extensions or tuple()
 
-    def load(fname):
+    def _load_and_process(self, fname, *args):
+        # Load the data, and merge with current scenario stack
+        scentime, scencmd = self.load(fname, *args)
+        # If there are no timestamps, process immediately
+        if not scentime:
+            process(zip(scencmd, len(scencmd) * [None]))
+            return True, f'Successfully loaded {fname}'
+        # If we have timestamps, merge the commands with the existing scenario stack
+        merge(zip(scentime, scencmd))
+        return True, f'Successfully loaded and merged {fname}'
+
+    def load(fname, *args):
         ''' Importer file loading.
         
             Arguments:
             - fname: Name of the file to load.
+            - args: Optional arguments to perform e.g., wildcard replacement
 
             Returns:
             - (scentime, scencmd): Tuple of two lists:
@@ -70,3 +68,5 @@ class Importer(Entity):
         super().__init_subclass__(replaceable=False, skipbase=True)
         # Then immediately instantiate
         cls._instance = cls()
+        # Add subcommand for this specific importer
+        Importer.importcmd.subcommand(cls._instance._load_and_process, name=cls._instance.filetype.upper())
