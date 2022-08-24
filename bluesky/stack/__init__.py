c7c270455f59e3a5c8fc71af27f68cc6f6555a5c
Joost Ellerbroek <jooste@users.noreply.github.com>
2022-08-24 15:59:22 +0200

    Update __init__.py

--- bluesky/stack/__init__.py
+++ bluesky/stack/__init__.py
@@ -2,7 +2,6 @@
 
     The stack parses all text-based commands in the simulation.
 '''
-from bluesky import settings
 from bluesky.stack.stackbase import stack, forward, sender, routetosender, get_scenname, get_scendata, set_scendata
 from bluesky.stack.cmdparser import command, commandgroup, append_commands, \
     remove_commands, get_commands
