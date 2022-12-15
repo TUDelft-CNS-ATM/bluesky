f8a94352914d651da1eba24f7a5b858791491d1b
Joost Ellerbroek <jooste@users.noreply.github.com>
2022-12-15 11:57:45 +0100

    Reverted change

--- bluesky/ui/qtgl/tiledtexture.py
+++ bluesky/ui/qtgl/tiledtexture.py
@@ -18,7 +18,7 @@ except ImportError:
     from PyQt6.QtGui import QImage, qRgba
 
 import bluesky as bs
-from bluesky.network import subscriber
+from bluesky.core import Signal
 from bluesky.ui.qtgl import glhelpers as glh
 
 
@@ -168,6 +168,7 @@ class TiledTexture(glh.Texture, metaclass=TiledTextureMeta):
         self.indexsampler_loc = 0
         self.arraysampler_loc = 0
         bs.net.actnodedata_changed.connect(self.actdata_changed)
+        Signal('panzoom').connect(self.panzoom)
 
     def add_bounding_box(self, lat0, lon0, lat1, lon1):
         ''' Add the bounding box of a textured shape.
@@ -256,7 +257,6 @@ class TiledTexture(glh.Texture, metaclass=TiledTextureMeta):
         if 'PANZOOM' in changed_elems:
             self.on_panzoom_changed(True)
 
-    @subscriber
     def panzoom(self, finished=False):
         ''' Update textures whenever pan/zoom changes. 
             
