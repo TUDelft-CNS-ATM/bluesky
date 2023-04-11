db7c89742038b6f44409817b2c18d15e40f47b1b
wy19990110 <49022199+wy19990110@users.noreply.github.com>
2023-04-11 14:43:45 +0800

    Update traffic.py
    Fixed a bug about the calculation of turn rate, where the original code did not match the calculation formula, resulting in an incorrect calculation of turn rate.

--- bluesky/traffic/traffic.py
+++ bluesky/traffic/traffic.py
@@ -449,8 +449,8 @@ class Traffic(Entity):
         # tan phi = a centrigugal/a grav = omega^2 * R / g = omega * V /g
         # => omega = (g tan phi)/V
         turnrate = np.degrees(g0 * np.tan(np.where(self.ap.turnphi>self.eps*self.eps,
-                                                   self.ap.turnphi,self.ap.bankdef) \
-                                          / np.maximum(self.tas, self.eps)))
+                                                   self.ap.turnphi,self.ap.bankdef)) \
+                                          / np.maximum(self.tas, self.eps))
         delhdg = (self.aporasas.hdg - self.hdg + 180) % 360 - 180  # [deg]
         self.swhdgsel = np.abs(delhdg) > np.abs(bs.sim.simdt * turnrate)
 
