9555bfe0ae54dc0c9654e7a8dcac382a80f9467e
Joost Ellerbroek <jooste@users.noreply.github.com>
2025-03-14 11:58:45 +0100

    Updated client example to new network api

--- extra/textclient/textclient.py
+++ extra/textclient/textclient.py
@@ -6,72 +6,50 @@
     PYTHONPATH=/path/to/your/bluesky python textclient.py
 '''
 from PyQt6.QtCore import Qt, QTimer
-from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QTextEdit
+from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QTextEdit, QLabel
 
+import bluesky as bs
+from bluesky.core import Base
+from bluesky.network import subscriber
 from bluesky.network.client import Client
+from bluesky.stack import stack
 
 
 # The echo textbox, command line, and bluesky network client as globals
 echobox = None
 cmdline = None
-bsclient = None
 
 
-class TextClient(Client):
-    '''
-        Subclassed Client with a timer to periodically check for incoming data,
-        an overridden event function to handle data, and a stack function to
-        send stack commands to BlueSky.
-    '''
-    def __init__(self):
-        super().__init__()
-        self.timer = QTimer()
-        self.timer.timeout.connect(self.update)
-        self.timer.start(20)
-
-    def event(self, name, data, sender_id):
-        ''' Overridden event function to handle incoming ECHO commands. '''
-        if name == b'ECHO' and echobox is not None:
-            echobox.echo(**data)
-
-    def stack(self, text):
-        ''' Stack function to send stack commands to BlueSky. '''
-        self.send(b'STACK', text)
-
-    def echo(self, text, flags=None):
-        ''' Overload Client's echo function. '''
-        if echobox is not None:
-            echobox.echo(text, flags)
-
-
-class Echobox(QTextEdit):
+class Echobox(QTextEdit, Base):
     ''' Text box to show echoed text coming from BlueSky. '''
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setMinimumHeight(150)
         self.setReadOnly(True)
-        # self.setFocusPolicy(Qt.NoFocus)
 
+    @subscriber
     def echo(self, text, flags=None):
         ''' Add text to this echo box. '''
         self.append(text)
         self.verticalScrollBar().setValue(self.verticalScrollBar().maximum())
 
+class InfoLine(QLabel, Base):
+    @subscriber
+    def acdata(self, data):
+        ''' Example subscriber to aircraft state data '''
+        self.setText(f"There are {len(data.lat)} aircraft in the simulation.")
 
 class Cmdline(QTextEdit):
     ''' Wrapper class for the command line. '''
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setMaximumHeight(21)
-        # self.setFocusPolicy(Qt.StrongFocus)
-        # self.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
 
     def keyPressEvent(self, event):
         ''' Handle Enter keypress to send a command to BlueSky. '''
         if event.key() == Qt.Key.Key_Enter or event.key() == Qt.Key.Key_Return:
-            if bsclient is not None:
-                bsclient.stack(self.toPlainText())
-                echobox.echo(self.toPlainText())
+            stack(self.toPlainText())
+            echobox.echo(self.toPlainText())
             self.setText('')
         else:
             super().keyPressEvent(event)
@@ -81,6 +59,14 @@ if __name__ == '__main__':
     # Construct the Qt main object
     app = QApplication([])
 
+    # Start the bluesky network client
+    bs.init(mode='client')
+    client = Client()
+    network_timer = QTimer()
+    network_timer.timeout.connect(client.update)
+    network_timer.start(20)
+    client.connect()
+
     # Create a window with a stack text box and a command line
     win = QWidget()
     win.setWindowTitle('Example external client for BlueSky')
@@ -89,13 +75,15 @@ if __name__ == '__main__':
 
     echobox = Echobox(win)
     cmdline = Cmdline(win)
+    infoline = InfoLine(win)
     layout.addWidget(echobox)
     layout.addWidget(cmdline)
+    layout.addWidget(infoline)
     win.show()
 
-    # Create and start BlueSky client
-    bsclient = TextClient()
-    bsclient.connect(recv_port=11000, send_port=11001)
+    # Let echobox act as screen object
+    # NOTE: this approach will soon be deprecated
+    bs.scr = echobox
 
     # Start the Qt main loop
     app.exec()
