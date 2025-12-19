34367678732a6cbc7c0f0801df4bbefdaa07309a
ProfHoekstra <ProfHoekstra@users.noreply.github.com>
2025-12-19 17:30:42 +0100

    Set up batch file for BlueSky on Windows updated.

--- setup-python.bat
+++ setup-python.bat
@@ -1,2 +1,4 @@
-pip install PyQt6 PyQt6-WebEngine pygame pyopengl numpy scipy pandas  matplotlib msgpack zmq textual bluesky-simdata bluesky-guidata 
+pip install PyQt6 PyQt6-WebEngine pyopengl numpy scipy pandas  matplotlib msgpack zmq textual pygame-ce
+
+pip install bluesky-simdata bluesky-guidata
 pause
